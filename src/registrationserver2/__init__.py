c92fb8b5826cff05292ae2019e57dad917a2687b
rifoerster <riccardo.foerster@sarad.de>
2020-11-20 09:32:32 +0100

    logging
    + Logging handles
    * logging still acts strange, one of the modules probably aggressivly overwrites things

--- src/registrationserver2/__init__.py
+++ src/registrationserver2/__init__.py
@@ -1,5 +1,6 @@
 
 import os
+import registrationserver2
 mainpy = fr"{os.path.dirname(os.path.realpath(__file__))}{os.path.sep}main.py"
 
 if __name__ == '__main__':
@@ -7,22 +8,40 @@ if __name__ == '__main__':
 	exit
 
 import logging
+
 import re
 from registrationserver2.config import config 
 
 config.setdefault('FOLDER', f'{os.environ.get("HOME",None) or os.environ.get("LOCALAPPDATA",None)}{os.path.sep}SARAD{os.path.sep}devices')
 config.setdefault('LEVEL', logging.CRITICAL)
-config.setdefault('MDNS_TIMEOUT', 3000)		
+config.setdefault('MDNS_TIMEOUT', 3000)	
+config.setdefault('TYPE', '_sarad-1688._rfc2217._tcp.local.')
 
 logging.basicConfig(level=logging.CRITICAL,format='[%(name)s]\t[%(levelname)s]:\t%(message)s')
 global theLogger
 theLogger = logging.getLogger('Instrument Server V2')
 logging.root.setLevel(logging.CRITICAL)
-theLogger.setLevel(config['LEVEL'])
-werklog = logging.getLogger('werkzeug')
-werklog.setLevel(logging.INFO)
-#werklog.disabled = True
+werklog = logging.getLogger('werkzeug')	
+formatter = logging.Formatter('[%(name)s]\t[%(levelname)s]:\t%(message)s')
+if werklog.handlers:
+	for handler in werklog.handlers:
+		werklog.removeHandler(handler)
+if theLogger.handlers:
+	for handler in theLogger.handlers:
+		theLogger.removeHandler(handler)
+		
+global streamh		
+streamh = logging.StreamHandler()
+logging.basicConfig(level=logging.CRITICAL, formatter=formatter)
+streamh.setFormatter(formatter)
+werklog.addHandler(streamh)
+werklog.addHandler(streamh)
 
+theLogger.setLevel(config['LEVEL'])
+werklog.setLevel(logging.CRITICAL) 
+
+
+theLogger.info("Test")
 
 #@TODO: move to config instead
 
