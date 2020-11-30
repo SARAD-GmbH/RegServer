c3f5e399b8cfbd5a65a780c4378547101cc4ec00
rifoerster <riccardo.foerster@sarad.de>
2020-11-30 12:42:55 +0100

    mdslistener is part of RFC2217 (for now)
    mdns listener now properly as a part of the rfc2217 module

--- src/registrationserver2/main.py
+++ src/registrationserver2/main.py
@@ -28,25 +28,18 @@ import threading
 import signal
 import os
 
-from registrationserver2.mdns_listener import SaradMdnsListener
 from registrationserver2.restapi import RestApi
 from registrationserver2.config import config
+import registrationserver2.modules
 
 
 
 if __name__ == '__main__':
-	if isinstance(config['TYPE'], list):
-		Test = []
-		for __type in config['TYPE']:
-			Test.append(SaradMdnsListener(_type = __type))
-	else:
-		Test = SaradMdnsListener(_type = config['TYPE'])
 	Test2 = RestApi()
 	apithread = threading.Thread(target=Test2.run,args=('0.0.0.0', 8000,))
 	apithread.start()
 	try:
 		input('Press Enter to End\n')
 	finally:
-		del Test
 		os.kill(os.getpid(), signal.SIGTERM) #Self kill, mostly to make sure all sub threads are stopped, including the rest API
 		
---
+++ src/registrationserver2/modules/rfc2217/__init__.py
@@ -0,0 +1,10 @@
+'''initialization of rfc2217 module, starting up mdns listener'''
+from registrationserver2 import config
+from registrationserver2.modules.rfc2217.mdns_listener import SaradMdnsListener
+
+if isinstance(config['TYPE'], list):
+	Test = []
+	for __type in config['TYPE']:
+		Test.append(SaradMdnsListener(_type = __type))
+else:
+	Test = SaradMdnsListener(_type = config['TYPE'])
