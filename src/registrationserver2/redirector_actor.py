5311ddbfd69310a6e6df3aa70c37bf29a3da3a01
rifoerster <riccardo.foerster@sarad.de>
2020-12-03 16:24:57 +0100

    redirector
    ~more work on the redirector

--- src/registrationserver2/redirector_actor.py
+++ src/registrationserver2/redirector_actor.py
@@ -4,13 +4,30 @@ Created on 01.12.2020
 @author: rfoerster
 '''
 import socket
+import traceback
+
+from dataclasses import dataclass
 
 from thespian.actors import Actor
 from registrationserver2.modules import device_base_actor
+from registrationserver2 import theLogger
+
+@dataclass
+class SockInfo:
+	'''Refer to SocketClient'''
+	address:str
+	port : int
+
+@dataclass
+class SocketClient:
+	''' Example: SocketClient(client_socket=<socket.socket fd=3708, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.0.1', 55224), raddr=('127.0.0.1', 56066)>, client_address=SockInfo(address='127.0.0.1', port=56066))'''
+	client_socket: socket
+	client_address: SockInfo
 
 class RedirectorActor(Actor):
 	'''Creating port for listening to Packages from a SARAD© Application'''
-	_sock : socket.socket
+	_sock : socket
+	_sockclient : SockInfo
 	_device : device_base_actor
 
 	ILLEGAL_STATE = {"ERROR":"Actor not setup correctly, make sure to send SETUP message first", "ERROR_CODE" : 5} # The actor was in an wrong state
@@ -21,7 +38,15 @@ class RedirectorActor(Actor):
 
 	def receive(self):
 		'''Listen to Port and redirect any messages'''
-		(client_socket, client_address) = self._sock.accept()
+		client_socket : socket
+		try:
+			client_socket = self._sock.accept()
+		except BaseException as error: #pylint: disable=W0703
+			theLogger.error(f'! {type(error)}\t{error}\t{vars(error) if isinstance(error, dict) else "-"}\t{traceback.format_exc()}')
+			client_socket = None
+			return
+		self._sockclient = SocketClient(client_socket[0],SockInfo(client_socket[1][0],client_socket[1][1]))
+		client_socket.close()
 
 	def receiveMessage(self, msg, sender): #pylint: disable=W0613,C0103 #@UnusedVariable
 		'''Actor receive message loop'''
@@ -50,6 +75,8 @@ class RedirectorActor(Actor):
 				_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 				_sock.bind(('',0)) #listen to any address on any available port
 				_sock.listen()
+				_sock.settimeout(0.5)
 			if not self._sock:
 				self.send(sender, self.ILLEGAL_STATE)
+
 			self.send(sender, { 'DATA' : self._sock.getsockname()}) #send back the actual used port
