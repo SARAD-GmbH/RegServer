8ef63dd7a6c918445f3d89084f34250102ccca1d
Michael Strey <mstrey@strey.biz>
2021-03-10 17:03:00 +0100

    +: introduce device actor manager
    This is maybe already in branch ManageDeviceActor.

---
+++ src/registrationserver2/modules/messages.py
@@ -0,0 +1,19 @@
+'''
+Created on 08.12.2020
+
+@author: rfoerster
+'''
+
+import logging
+
+logging.getLogger('Registration Server V2').info(f'{__package__}->{__file__}')
+
+RETURN_MESSAGES = {
+		'ILLEGAL_WRONGFORMAT' : {"ERROR":"Misformatted or no message sent", "ERROR_CODE" : 1}, # The message received by the actor was not in an expected format
+		'ILLEGAL_NOTIMPLEMENTED' : {"ERROR":"Not implemented", "ERROR_CODE" : 2}, #The command received by the actor was not yet implemented by the implementing class
+		'ILLEGAL_WRONGTYPE' : {"ERROR":"Wrong Message Type, dictionary Expected", "ERROR_CODE" : 3}, # The message received by the actor was not in an expected type
+		'ILLEGAL_UNKNOWN_COMMAND' : {"ERROR":"Unknown Command", "ERROR_CODE" : 4}, # The message received by the actor was not in an expected type
+		'ILLEGAL_STATE' : {"ERROR":"Actor not setup correctly, make sure to send SETUP message first", "ERROR_CODE" : 5}, # The actor was in an wrong state
+		'OK_SKIPPED' : { "RETURN" : True, "SKIPPED": True },
+		'OK' : { "RETURN" : True, "SKIPPED": True}
+	}
