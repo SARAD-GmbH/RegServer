2ae77ba1f6732b3259d8b6a84f118412fb33faf6
Michael Strey <strey@sarad.de>
2021-06-14 15:48:28 +0200

    +: Powershell script for deployment
    with Radon Vision

--- copy-venv.ps1
+++ copy-venv.ps1
@@ -1,5 +1,11 @@
+$target = "Y:\Software\Sarad_dev\RadonVision"
 $venvoutput = pipenv --venv
-$sites = (Get-Item $venvoutput[0]).FullName + "\Lib\site-packages"
-$src =  (Get-Item $venvoutput[0]).FullName + "\src" 
-Copy-Item $sites .\temp\bin -Force
-Copy-Item $src\* .\temp\src -Force
+$target_bin = "$($target)\bin\"
+$target_packages = "$($target)\site-packages\"
+$source_packages1 = (Get-Item $venvoutput[0]).FullName + "\Lib\site-packages\*"
+$source_packages2 = (Get-Item $venvoutput[0]).FullName + "\src\data-collector\sarad\"
+$source_bin = ".\src\"
+Get-ChildItem -Path $source_packages1 -Directory | Copy-Item -Destination $target_packages -Force -Recurse
+Copy-Item -Path $source_packages2 -Destination $target_packages -Force -Recurse
+Get-ChildItem -Path $source_bin -File | Copy-Item -Destination $target_bin -Force
+Get-ChildItem -Path $source_bin -Directory | Copy-Item -Destination $target_packages -Force -Recurse
