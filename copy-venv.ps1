3c01cee1d9ff0e218646fcf0a6e58904bca4231e
Michael Strey <strey@sarad.de>
2021-06-15 11:56:36 +0200

    !: add required files from root of site-packages

--- copy-venv.ps1
+++ copy-venv.ps1
@@ -6,6 +6,7 @@ $source_packages1 = (Get-Item $venvoutput[0]).FullName + "\Lib\site-packages\*"
 $source_packages2 = (Get-Item $venvoutput[0]).FullName + "\src\data-collector\sarad\"
 $source_bin = ".\src\"
 Get-ChildItem -Path $source_packages1 -Directory | Copy-Item -Destination $target_packages -Force -Recurse
+Get-ChildItem -Path $source_packages1 -File | ? {$_.Name -notlike "*virtualenv*"} | Copy-Item -Destination $target_packages -Force
 Copy-Item -Path $source_packages2 -Destination $target_packages -Force -Recurse
 Get-ChildItem -Path $source_bin -File | Copy-Item -Destination $target_bin -Force
 Get-ChildItem -Path $source_bin -Directory | Copy-Item -Destination $target_packages -Force -Recurse
