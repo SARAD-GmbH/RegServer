a55a57b278392a60a777a76cfb8d2568d1b2b972
Michael Strey <mstrey@strey.biz>
2021-09-17 12:30:44 +0200

    Docs: Introduce Google style docstrings

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,9 +18,9 @@ sys.path.insert(0, os.path.abspath("../../src/"))
 
 # -- Project information -----------------------------------------------------
 
-project = "Registration Server"
-copyright = "2021, Riccardo Förster"
-author = "Riccardo Förster"
+project = "SARAD Registration Server"
+copyright = "2021, SARAD GmbH"
+author = "Riccardo Förster <riccardo.foerster@sarad.de>, Michael Strey <strey@sarad.de>"
 
 # The full version, including alpha/beta/rc tags
 release = "0.1"
@@ -31,7 +31,7 @@ release = "0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinxcontrib.plantuml"]
+extensions = ["sphinx.ext.autodoc", "sphinxcontrib.plantuml", "sphinx.ext.napoleon"]
 # extensions.append('autoapi.extension')
 # extensions.append('sphinx_autodoc_typehints')
 
@@ -46,7 +46,6 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -57,4 +56,20 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
