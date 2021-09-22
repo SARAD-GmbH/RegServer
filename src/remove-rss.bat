d966f6d31c1c870a3b6a8c40aa82d4324e27968e
Michael Strey <strey@sarad.de>
2021-09-22 13:27:41 +0200

    !: batch files for service start and removal

--- src/remove-rss.bat
+++ src/remove-rss.bat
@@ -1,8 +1,3 @@
 cd /D "%~dp0
 .\regserver-service.exe stop
-:loop
-	timeout /T 10 /nobreak
-	for /f %%a in ('.\regserver-service.exe status') do set REPLY=%%a
-        echo %REPLY%
-	if %REPLY% neq S goto loop
 .\regserver-service.exe remove
