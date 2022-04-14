edef4030419d089826f410e963490366703fc422
Michael Strey <mstrey@strey.biz>
2022-04-14 16:55:55 +0200

    +: is1 listener

--- src/registrationserver/modules/is1/is1_listener.py
+++ src/registrationserver/modules/is1/is1_listener.py
@@ -1,161 +1,85 @@
-"""Listening for mDNS multicast messages announcing the existence of new
-services (SARAD devices) in the local network.
+"""Listening for notifications from Instrument Server 1
 
 :Created:
-    2020-09-30
+    2022-04-14
 
 :Authors:
-    | Riccardo Förster <foerster@sarad.de>,
     | Michael Strey <strey@sarad.de>
-
-.. uml:: uml-mdns_listener.puml
-
 """
-import ipaddress
-import json
+import datetime
+import select
 import socket
-import threading
 
-import hashids  # type: ignore
+from overrides import overrides  # type: ignore
 from registrationserver.actor_messages import (ActorCreatedMsg, CreateActorMsg,
                                                SetDeviceStatusMsg)
+from registrationserver.base_actor import BaseActor
 from registrationserver.config import config
 from registrationserver.helpers import get_actor
 from registrationserver.logger import logger
-from registrationserver.modules.rfc2217.rfc2217_actor import Rfc2217Actor
 from registrationserver.shutdown import system_shutdown
-from thespian.actors import ActorExitRequest, ActorSystem  # type: ignore
-from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
 
 logger.debug("%s -> %s", __package__, __file__)
 
 
-class MdnsListener(ServiceListener):
-    """Listens for mDNS multicast messages
+class Is1Listener(BaseActor):
+    """Listens for messages from Instrument Server 1
 
-    * adds new services (SARAD Instruments) to the system by creating a device actor
-    * updates services with the latest info from mDNS multicast messages
-    * removes services when they disapear from the network
+    * adds new SARAD Instruments to the system by creating a device actor
     """
 
-    @staticmethod
-    def convert_properties(info=None, name=""):
-        """Helper function to convert mdns service information
-        to the desired yaml format"""
-        if not info or not name:
-            return None
-        properties = info.properties
-        if properties is not None:
-            _model = properties.get(b"MODEL_ENC", None)
-            if _model is None:
-                return None
-        _model = _model.decode("utf-8")
-        _serial_short = properties.get(b"SERIAL_SHORT", None)
-        if _serial_short is None:
-            return None
-        _device_id = _serial_short.decode("utf-8").split(".")[0]
-        _sarad_protocol = _serial_short.decode("utf-8").split(".")[1]
-        hids = hashids.Hashids()
-        _ids = hids.decode(_device_id)
-        if _ids is None:
-            return None
-        if (len(_ids) != 3) or not info.port:
-            return None
-        _addr = ""
-        try:
-            _addr_ip = ipaddress.IPv4Address(info.addresses[0]).exploded
-            _addr = socket.gethostbyaddr(_addr_ip)[0]
-        except Exception:  # pylint: disable=broad-except
-            logger.critical("Fatal error")
-            system_shutdown()
-        out = {
-            "Identification": {
-                "Name": properties[b"MODEL_ENC"].decode("utf-8"),
-                "Family": _ids[0],
-                "Type": _ids[1],
-                "Serial number": _ids[2],
-                "Host": _addr,
-                "Protocol": _sarad_protocol,
-            },
-            "Remote": {"Address": _addr_ip, "Port": info.port, "Name": name},
-        }
-        logger.debug(out)
-        return json.dumps(out)
-
-    @staticmethod
-    def get_ip():
-        """Find my own IP address"""
-        test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            # doesn't even have to be reachable
-            test_socket.connect(("10.255.255.255", 1))
-            ip_address = test_socket.getsockname()[0]
-        except Exception:  # pylint: disable=broad-except
-            ip_address = "127.0.0.1"
-        finally:
-            test_socket.close()
-        logger.debug("My IP address is %s", ip_address)
-        return ip_address
-
-    def __init__(self, registrar_actor, service_type):
-        """
-        Initialize a mdns Listener for a specific device group
-        """
-        self.registrar = registrar_actor
-        self.lock = threading.Lock()
-        with self.lock:
-            self.zeroconf = Zeroconf(
-                ip_version=config["IP_VERSION"], interfaces=[self.get_ip(), "127.0.0.1"]
-            )
-            _ = ServiceBrowser(self.zeroconf, service_type, self)
-
-    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        # pylint: disable=invalid-name
-        """Hook, being called when a new service
-        representing a device is being detected"""
-        with self.lock:
-            logger.info("[Add] Found service %s of type %s", name, type_)
-            info = zc.get_service_info(type_, name, timeout=config["MDNS_TIMEOUT"])
-            if info is not None:
-                logger.info("[Add] %s", info.properties)
-                # Take the first 3 elements to form a short_name
-                actor_id = ".".join(name.split(".", 3)[:-1])
-                reply = ActorSystem().ask(
-                    self.registrar, CreateActorMsg(Rfc2217Actor, actor_id)
-                )
-                if not isinstance(reply, ActorCreatedMsg):
-                    logger.critical("Got message object of unexpected type")
-                    logger.critical("-> Stop and shutdown system")
-                    system_shutdown()
-                else:
-                    device_actor = reply.actor_address
-                    data = self.convert_properties(name=name, info=info)
-                    logger.debug("Setup the device actor with %s", data)
-                    ActorSystem().tell(device_actor, SetDeviceStatusMsg(data))
-
-    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        # pylint: disable=invalid-name
-        """Hook, being called when a service
-        representing a device is being updated"""
-        logger.info("[Update] Service %s of type %s", name, type_)
-        self.add_service(zc, type_, name)
-
-    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        # pylint: disable=invalid-name
-        """Hook, being called when a regular shutdown of a service
-        representing a device is being detected"""
-        with self.lock:
-            logger.info("[Del] Service %s of type %s", name, type_)
-            info = zc.get_service_info(type_, name, timeout=config["MDNS_TIMEOUT"])
-            logger.debug("[Del] Info: %s", info)
+    @overrides
+    def __init__(self):
+        super().__init__()
+        self.my_parent = None
+        self._client_socket = None
+        self._socket_info = None
+        self.conn = None
+        self._host = config["HOST"]
+        logger.debug("IP address of Registration Server: %s", self._host)
+        for self._port in [50002]:
             try:
-                device_actor = get_actor(self.registrar, name)
-            except KeyError:
-                logger.critical("The actor to remove does not exist.")
-                system_shutdown()
-            logger.debug("Kill the device actor...")
-            ActorSystem().tell(device_actor, ActorExitRequest())
+                server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+                server_socket.bind((self._host, self._port))
+                self._port = server_socket.getsockname()[1]
+                break
+            except OSError:
+                try:
+                    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    server_socket.bind((self._host, self._port))
+                    self._port = server_socket.getsockname()[1]
+                    break
+                except OSError as exception:
+                    logger.error("Cannot use port %d. %s", self._port, exception)
+                    server_socket.close()
+        try:
+            server_socket.listen()  # listen(5) maybe???
+        except OSError:
+            self._port = None
+        logger.debug("Server socket: %s", server_socket)
+        self.read_list = [server_socket]
+        if self._port is not None:
+            logger.info("Socket listening on %s:%d", self._host, self._port)
 
-    def shutdown(self) -> None:
-        """Cleanup"""
-        self.zeroconf.close()
+    def listen(self):
+        """Listen for notification from Instrument Server"""
+        server_socket = self.read_list[0]
+        timeout = 0.1
+        readable, _writable, _errored = select.select(self.read_list, [], [], timeout)
+        for self.conn in readable:
+            if self.conn is server_socket:
+                self._client_socket, self._socket_info = server_socket.accept()
+                self.read_list.append(self._client_socket)
+                logger.debug("Connection from %s", self._socket_info)
+            else:
+                self._cmd_handler()
+        self.wakeupAfter(datetime.timedelta(seconds=0.01), payload="Connect")
+
+    def receiveMsg_WakeupMessage(self, msg, _sender):
+        # pylint: disable=invalid-name
+        """Handler for WakeupMessage"""
+        if msg.payload == "Connect":
+            self.listen()
+
+    def _cmd_handler(self):
+        """Handle a binary SARAD command received via the socket."""
