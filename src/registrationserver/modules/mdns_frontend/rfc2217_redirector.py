b47e2b926877ef75594d7834147154ceb9f878b9
Michael Strey <mstrey@strey.biz>
2022-05-09 14:03:17 +0200

    ?: first attempt of data transmission via mDNS frontend

--- src/registrationserver/modules/mdns_frontend/rfc2217_redirector.py
+++ src/registrationserver/modules/mdns_frontend/rfc2217_redirector.py
@@ -11,20 +11,22 @@ Based on an example of Chris Liechti <cliechti@gmx.net>.
 
 """
 
+import datetime
 import socket
 import time
 from typing import List
 
-import serial.rfc2217
 from overrides import overrides  # type: ignore
 from registrationserver.actor_messages import (GetDeviceStatusMsg,
-                                               SetupMdnsAdvertiserActorMsg)
+                                               SetupMdnsAdvertiserActorMsg,
+                                               TxBinaryMsg)
 from registrationserver.base_actor import BaseActor
 from registrationserver.config import mdns_frontend_config
 from registrationserver.helpers import short_id
 from registrationserver.logger import logger
 from registrationserver.modules.mdns_frontend.mdns_advertiser import \
     MdnsAdvertiserActor
+from serial.serialutil import iterbytes  # type: ignore
 
 
 class Rfc2217RedirectorActor(BaseActor):
@@ -35,8 +37,33 @@ class Rfc2217RedirectorActor(BaseActor):
     _available_ports: List[int] = []
 
     @staticmethod
+    def escape(data):
+        """This generator function is for the user. All outgoing data has to be
+        properly escaped, so that no IAC character in the data stream messes up
+        the Telnet state machine in the server.
+        socket.sendall(escape(data))
+        """
+        iac = b"\xff"  # Interpret As Command
+        for byte in iterbytes(data):
+            if byte == iac:
+                yield iac
+                yield iac
+            else:
+                yield byte
+
+    @staticmethod
     def _advertiser(instr_id):
         return f"advertiser-{instr_id}"
+
+    @staticmethod
+    def create_socket(port):
+        """Open listening TCP/IP socket and return its port number"""
+        srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        srv.bind(("", port))
+        srv.listen(1)
+        srv.setblocking(0)
+        return srv
 
     @classmethod
     def set_port_range_initialised(cls):
@@ -52,6 +79,9 @@ class Rfc2217RedirectorActor(BaseActor):
             self._available_ports = list(self.PORT_RANGE)
             if not self._available_ports:
                 logger.critical("No more available ports")
+        self.s_socket = None
+        self.conn = None
+        self._socket_info = None
 
     def receiveMsg_SetupRfc2217RedirectorMsg(self, msg, sender):
         # pylint: disable=invalid-name
@@ -65,7 +95,13 @@ class Rfc2217RedirectorActor(BaseActor):
         """Handler for UpdateDeviceStatusMsg from Device Actor."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
         instr_id = short_id(msg.device_id)
-        if self.open_socket():
+        try:
+            self._selected_port = self._available_ports.pop()
+            self.s_socket = self.create_socket(self._selected_port)
+        except Exception as exception:  # pylint: disable=broad-except
+            logger.error("Cannot create listening socket on %d", self._selected_port)
+            logger.error(exception)
+        if self.s_socket is not None:
             my_advertiser = self._create_actor(
                 MdnsAdvertiserActor, self._advertiser(instr_id)
             )
@@ -75,14 +111,79 @@ class Rfc2217RedirectorActor(BaseActor):
                     device_actor=self.device_actor, tcp_port=self._selected_port
                 ),
             )
+            self._loop()
+
+    def receiveMsg_WakeupMessage(self, msg, _sender):
+        # pylint: disable=invalid-name
+        """Handler for WakeupMessage"""
+        if msg.payload == "Connect" and not self.on_kill:
+            self._loop()
+
+    def _loop(self):
+        """Listen to socket and redirect any message from the socket to the device actor"""
+        # logger.debug("%s for %s from %s", msg, self.my_id, sender)
+        # read_list = list of server sockets from which we expect to read
+        try:
+            self.conn, self._socket_info = self.s_socket.accept()
+        except BlockingIOError:
+            time.sleep(0.5)
+            return
+        logger.debug("Connected by %s:%s", self._socket_info[0], self._socket_info[1])
+        self.conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        try:
+            self._cmd_handler()
+        finally:
+            try:
+                self.conn.shutdown(socket.SHUT_RDWR)
+            except OSError:
+                logger.warning("socket shutdown error")
+            self.conn.close()
+        self.wakeupAfter(datetime.timedelta(seconds=0.01), payload="Connect")
+
+    def _cmd_handler(self):
+        """Handle a binary SARAD command received via the socket."""
+        for _i in range(0, 5):
+            try:
+                data = self.conn.recv(1024)
+                break
+            except (ConnectionResetError, BrokenPipeError):
+                logger.error("Connection reset by SARAD application software.")
+                data = None
+                time.sleep(5)
+        if data is None:
+            logger.critical("Application software seems to be dead.")
+            self.receiveMsg_KillMsg(None, self.parent)
+        elif data == b"":
+            logger.debug("The application closed the socket.")
+            self.receiveMsg_KillMsg(None, self.parent)
+        else:
+            logger.debug(
+                "Redirect %s from app, socket %s to device actor %s",
+                data,
+                self._socket_info,
+                self.device_actor,
+            )
+            self.send(self.device_actor, TxBinaryMsg(data))
+
+    def receiveMsg_RxBinaryMsg(self, msg, sender):
+        # pylint: disable=invalid-name
+        """Redirect any received reply to the socket."""
+        logger.debug("%s for %s from %s", msg, self.my_id, sender)
+        data = b"".join(self.escape(msg.data))
+        for _i in range(0, 5):
+            try:
+                self.conn.sendall(data)
+                return
+            except (ConnectionResetError, BrokenPipeError):
+                logger.error("Connection reset by SARAD application software.")
+                time.sleep(5)
+        logger.critical("Application software seems to be dead.")
+        self.receiveMsg_KillMsg(None, self.parent)
 
     @overrides
     def receiveMsg_KillMsg(self, msg, sender):
         if self._isPortRangeInit:
             self._available_ports.append(self._selected_port)
+        self.s_socket.shutdown(socket.SHUT_RDWR)
+        self.s_socket.close()
         super().receiveMsg_KillMsg(msg, sender)
-
-    def open_socket(self):
-        """Open listening TCP/IP socket and return its port number"""
-        self._selected_port = self._available_ports.pop()
-        return True
