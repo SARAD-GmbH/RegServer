SARAD-GmbH/RegServer

commit 16d44b7a7a06e7457c7e70de891c8088886bff52

+: change type of 'success' in _forward_reservation()
from bool to Status.
+: implement reservation in mDNS backend

src/registrationserver/modules/device_actor.py
@@ -20,6 +20,7 @@ from registrationserver.base_actor import BaseActor
 from registrationserver.helpers import short_id
 from registrationserver.logger import logger
 from registrationserver.redirect_actor import RedirectorActor
+from registrationserver.shutdown import system_shutdown
 
 logger.debug("%s -> %s", __package__, __file__)
 
@@ -107,18 +108,22 @@ class DeviceBaseActor(BaseActor):
             self.user: String identifying the user of the app.
         """
 
-    def _forward_reservation(self, success: bool):
+    def _forward_reservation(self, success: Status):
         # pylint: disable=unused-argument, no-self-use
         """Create redirector.
         Forward the reservation state from the Instrument Server to the REST API.
         This function has to be called in the protocol specific modules.
         """
-        if success:
+        self.send(self.sender_api, ReservationStatusMsg(success))
+        if success in [Status.OK, Status.OK_UPDATED, Status.OK_SKIPPED]:
             # create redirector
             if not self._create_redirector():
-                logger.error("Tried to create a redirector that already exists.")
-            return
-        self.send(self.sender_api, ReservationStatusMsg(Status.OCCUPIED))
+                logger.warning("Tried to create a redirector that already exists.")
+        elif success in [Status.NOT_FOUND, Status.IS_NOT_FOUND]:
+            self.send(self.myAddress, KillMsg())
+        elif success == Status.ERROR:
+            logger.critical("%s during reservation", success)
+            system_shutdown()
 
     def _create_redirector(self) -> bool:
         """Create redirector actor if it does not exist already"""

src/registrationserver/modules/backend/mdns/device_actor.py
@@ -12,8 +12,10 @@
 import socket
 import time
 
+import requests
 from overrides import overrides  # type: ignore
-from registrationserver.actor_messages import KillMsg, RxBinaryMsg
+from registrationserver.actor_messages import KillMsg, RxBinaryMsg, Status
+from registrationserver.config import config
 from registrationserver.logger import logger
 from registrationserver.modules.device_actor import DeviceBaseActor
 
@@ -136,4 +138,16 @@ class DeviceActor(DeviceBaseActor):
         TODO: Read the reply from the REST API of the Instrument Server.
         In this dummy we suppose, that the instrument is always available for us.
         """
-        self._forward_reservation(True)
+        base_url = f'http://{self._is_host}:{config["API_PORT"]}'
+        device_id = self.my_id
+        app = f"{self.app} - {self.user}"
+        resp = requests.get(f"{base_url}/list/{device_id}/reserve", {"who": app})
+        if resp.status_code != 200:
+            success = Status.IS_NOT_FOUND
+        else:
+            logger.debug(resp.json())
+            try:
+                success = Status(resp.json()["Error code"])
+            except KeyError:
+                success = Status.ERROR
+        self._forward_reservation(success)

Pipfile
@@ -28,6 +28,7 @@ polling2 = "*"
 pefile = "*"
 pyinstaller = "*"
 toml = "*"
+requests = "*"
 
 [requires]
 python_version = "3"

src/config_linux.toml
@@ -5,7 +5,7 @@ port_range = [50000, 50500]
 native_serial_ports = []
 ignored_serial_ports = []
 local_retry_interval = 30  # in seconds
-api_port = 8000
+api_port = 8008
 #host = "::1"  # Localhost IPv6
 # host = "192.168.10.19"  # Michael@HomeVPN
 host = "ap-strey.hq.sarad.de"  # Michael@Work

src/config_windows.toml
@@ -5,7 +5,7 @@ port_range = [50000, 50500]
 native_serial_ports = ["COM1"]
 ignored_serial_ports = []
 local_retry_interval = 86400  # in seconds
-api_port = 8000
+api_port = 8008
 host = "::1"  # Localhost IPv6
 
 [frontends]

src/registrationserver/actor_messages.py
@@ -9,7 +9,7 @@ commands and data within the actor system
 
 """
 from dataclasses import dataclass
-from enum import Enum, auto, unique
+from enum import Enum, unique
 from typing import Any, ByteString, Dict, List, Union
 
 from sarad.sari import FamilyDict  # type: ignore
@@ -25,11 +25,14 @@ class Status(Enum):
     OCCUPIED = 6
     OK_SKIPPED = 10
     NOT_FOUND = 11
+    IS_NOT_FOUND = 12
     ATTRIBUTE_ERROR = 13
+    OTHER_APP_USER = 14
     OK_UPDATED = 20
     SUBSCRIBE = 34
     UNSUBSCRIBE = 35
     UNKNOWN_PORT = 40
+    ERROR = 98
     CRITICAL = 99
 
     def __str__(self):
@@ -38,11 +41,14 @@ class Status(Enum):
             6: "Device occupied",
             10: "OK, skipped",
             11: "Device not found.",
+            12: "Instrument server not found",
             13: "No or incomplete attributes",
+            14: "Instrument reserved for other app or other user",
             20: "OK, updated",
             34: "Error when subscribing to an MQTT topic",
             35: "Error when unsubscribing from an MQTT topic",
             40: "Port does not exist.",
+            98: "Unknown error",
             99: "Critical error. Stop and shutdown system.",
         }
         return longform[self.value]

src/registrationserver/config.py
@@ -54,7 +54,7 @@ else:
     DEFAULT_NATIVE_SERIAL_PORTS = ["/dev/ttyS0"]
 DEFAULT_IGNORED_SERIAL_PORTS: List[str] = []
 DEFAULT_LOCAL_RETRY_INTERVAL = 30  # in seconds
-DEFAULT_API_PORT = 8000
+DEFAULT_API_PORT = 8008
 DEFAULT_HOST = "localhost"
 level_dict = {
     "info": logging.INFO,

src/registrationserver/modules/backend/is1/is1_actor.py
@@ -11,7 +11,7 @@ import socket
 import time
 
 from overrides import overrides  # type: ignore
-from registrationserver.actor_messages import KillMsg, RxBinaryMsg
+from registrationserver.actor_messages import KillMsg, RxBinaryMsg, Status
 from registrationserver.helpers import check_message, make_command_msg
 from registrationserver.logger import logger
 from registrationserver.modules.device_actor import DeviceBaseActor
@@ -133,7 +133,7 @@ class Is1Actor(DeviceBaseActor):
         """Reserve the requested instrument at the instrument server."""
         if not self._establish_socket():
             logger.error("Can't establish the client socket.")
-            self._forward_reservation(False)
+            self._forward_reservation(Status.IS_NOT_FOUND)
             return
         cmd_msg = make_command_msg(
             [self.SELECT_COM, (self._com_port).to_bytes(1, byteorder="little")]
@@ -143,7 +143,7 @@ class Is1Actor(DeviceBaseActor):
             reply = self._socket.recv(1024)
         except TimeoutError:
             logger.error("Timeout on waiting for reply to SELECT_COM: %s", cmd_msg)
-            self._forward_reservation(False)
+            self._forward_reservation(Status.IS_NOT_FOUND)
             self._destroy_socket()
             return
         checked_reply = check_message(reply, multiframe=False)
@@ -153,14 +153,14 @@ class Is1Actor(DeviceBaseActor):
             and checked_reply["payload"][0].to_bytes(1, byteorder="little")
             == self.COM_SELECTED
         ):
-            self._forward_reservation(True)
+            self._forward_reservation(Status.OK)
         elif (
             checked_reply["is_valid"]
             and checked_reply["payload"] == self.COM_NOT_AVAILABLE
         ):
             self.send(self.myAddress, KillMsg())
         else:
-            self._forward_reservation(False)
+            self._forward_reservation(Status.NOT_FOUND)
         self._destroy_socket()
 
     @overrides

commit 631ad632407ef740f6a158bc83c6ba2b1519d5c2

!: regression from 16d44b7a7a
http://r2d2.hq.sarad.de:3000/SARAD/RegServer/commit/16d44b7a7a06e7457c7e70de891c8088886bff52

src/registrationserver/modules/device_actor.py
@@ -114,12 +114,12 @@ class DeviceBaseActor(BaseActor):
         Forward the reservation state from the Instrument Server to the REST API.
         This function has to be called in the protocol specific modules.
         """
-        self.send(self.sender_api, ReservationStatusMsg(success))
         if success in [Status.OK, Status.OK_UPDATED, Status.OK_SKIPPED]:
             # create redirector
             if not self._create_redirector():
                 logger.warning("Tried to create a redirector that already exists.")
-        elif success in [Status.NOT_FOUND, Status.IS_NOT_FOUND]:
+            return
+        if success in [Status.NOT_FOUND, Status.IS_NOT_FOUND]:
             logger.error(
                 "Reservation failed with %s. Removing device from list.", success
             )
@@ -127,6 +127,7 @@ class DeviceBaseActor(BaseActor):
         elif success == Status.ERROR:
             logger.critical("%s during reservation", success)
             system_shutdown()
+        self.send(self.sender_api, ReservationStatusMsg(success))
 
     def _create_redirector(self) -> bool:
         """Create redirector actor if it does not exist already"""