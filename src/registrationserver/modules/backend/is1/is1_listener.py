SARAD-GmbH/RegServer

commit 0c7d9de80a0d5f1c1603ce1c3636c2ed67648711

!: Re-scan in IS1 conflicts with TX/RX
Workaround for shortcommings of IS1 implementation.

src/registrationserver/modules/backend/is1/is1_listener.py
@@ -134,6 +134,8 @@ class Is1Listener(BaseActor):
             logger.info("Socket listening on %s:%d", my_ip, self._port)
         self.instrument_servers = set()
         self.pickle_file_name = f"{app_folder}wlan_instruments.pickle"
+        self.allow_rescan = True
+        self.last_activity = datetime.datetime.utcnow()
 
     @overrides
     def receiveMsg_SetupMsg(self, msg, sender):
@@ -156,13 +158,14 @@ class Is1Listener(BaseActor):
         """Handler for RescanMessage causing a prompt re-scan
         on all known instrument servers"""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
-        instrument_servers = list(self.instrument_servers)
-        for instrument_server in instrument_servers:
-            logger.debug(
-                "Check %s for living instruments",
-                instrument_server.host,
-            )
-            self._scan_is(instrument_server)
+        if self.allow_rescan:
+            instrument_servers = list(self.instrument_servers)
+            for instrument_server in instrument_servers:
+                logger.debug(
+                    "Check %s for living instruments",
+                    instrument_server.host,
+                )
+                self._scan_is(instrument_server)
         self.wakeupAfter(datetime.timedelta(seconds=60), payload="Rescan")
 
     def listen(self):
@@ -191,6 +194,21 @@ class Is1Listener(BaseActor):
             self.listen()
         if msg.payload == "Rescan" and not self.on_kill:
             self.receiveMsg_RescanMsg(msg, sender)
+
+    def receiveMsg_AllowRescanMsg(self, msg, sender):
+        # pylint: disable=invalid-name
+        """Allow or forbid performing the self._can_is() function.
+
+        Workaround for the shortcommings of IS1 implementation on WLAN module."""
+        logger.debug("%s for %s from %s", msg, self.my_id, sender)
+        self.allow_rescan = bool(
+            msg.allow
+            and (
+                (datetime.datetime.utcnow() - self.last_activity)
+                > datetime.timedelta(seconds=60)
+            )
+        )
+        self.last_activity = datetime.datetime.utcnow()
 
     def _cmd_handler(self, is_host):
         """Handle a binary SARAD command received via the socket."""

src/registrationserver/actor_messages.py
@@ -488,3 +488,11 @@ class ReturnDeviceActorMsg:
     requested with GetDeviceActorMsg."""
 
     actor_address: ActorAddress
+
+
+@dataclass
+class AllowRescanMsg:
+    """Message sent from the IS1 device actor to IS1 listener to allow/forbid the
+    re-scan of instrument server"""
+
+    allow: bool

src/registrationserver/modules/backend/is1/is1_actor.py
@@ -11,7 +11,8 @@ import socket
 import time
 
 from overrides import overrides  # type: ignore
-from registrationserver.actor_messages import KillMsg, RxBinaryMsg, Status
+from registrationserver.actor_messages import (AllowRescanMsg, KillMsg,
+                                               RxBinaryMsg, Status)
 from registrationserver.helpers import check_message, make_command_msg
 from registrationserver.logger import logger
 from registrationserver.modules.device_actor import DeviceBaseActor
@@ -108,6 +109,7 @@ class Is1Actor(DeviceBaseActor):
     def receiveMsg_TxBinaryMsg(self, msg, sender):
         # pylint: disable=invalid-name
         """Handler for TxBinaryMsg from App to Instrument."""
+        self.send(self.parent.parent_address, AllowRescanMsg(False))
         super().receiveMsg_TxBinaryMsg(msg, sender)
         if not self._establish_socket():
             logger.error("Can't establish the client socket.")
@@ -130,6 +132,7 @@ class Is1Actor(DeviceBaseActor):
             self.send(self.myAddress, KillMsg())
             reply = b""
         self.send(self.redirector_actor, RxBinaryMsg(reply))
+        self.send(self.parent.parent_address, AllowRescanMsg(True))
 
     @overrides
     def _reserve_at_is(self):

commit 230b11ea84a04ad33ea0c849ea63884ab154a48c

!: regression from 0c7d9de80a
http://r2d2.hq.sarad.de:3000/SARAD/RegServer/commit/0c7d9de80a0d5f1c1603ce1c3636c2ed67648711

src/registrationserver/modules/backend/is1/is1_listener.py
@@ -151,14 +151,24 @@ class Is1Listener(BaseActor):
             instrument_server.instruments = frozenset([])
         self._subscribe_to_actor_dict_msg()
         self.wakeupAfter(datetime.timedelta(seconds=0.01), payload="Connect")
-        self.wakeupAfter(datetime.timedelta(seconds=1), payload="Rescan")
+        self.wakeupAfter(datetime.timedelta(seconds=20), payload="Rescan")
 
     def receiveMsg_RescanMsg(self, msg, sender):
         # pylint: disable=invalid-name
         """Handler for RescanMessage causing a prompt re-scan
         on all known instrument servers"""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
-        if self.allow_rescan:
+        time_condition = bool(
+            datetime.datetime.utcnow() - self.last_activity
+            > datetime.timedelta(seconds=10)
+        )
+        allow_rescan = bool(self.allow_rescan and time_condition)
+        logger.debug(
+            "self.allow_rescan = %s, time_condition = %s",
+            self.allow_rescan,
+            time_condition,
+        )
+        if allow_rescan:
             instrument_servers = list(self.instrument_servers)
             for instrument_server in instrument_servers:
                 logger.debug(
@@ -197,17 +207,11 @@ class Is1Listener(BaseActor):
 
     def receiveMsg_AllowRescanMsg(self, msg, sender):
         # pylint: disable=invalid-name
-        """Allow or forbid performing the self._can_is() function.
+        """Allow or forbid performing the self._scan_is() function.
 
         Workaround for the shortcommings of IS1 implementation on WLAN module."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
-        self.allow_rescan = bool(
-            msg.allow
-            and (
-                (datetime.datetime.utcnow() - self.last_activity)
-                > datetime.timedelta(seconds=60)
-            )
-        )
+        self.allow_rescan = msg.allow
         self.last_activity = datetime.datetime.utcnow()
 
     def _cmd_handler(self, is_host):