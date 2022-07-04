SARAD-GmbH/RegServer

commit 76f77152d55de0689a715380ff0a8f8767e5c82b

!: endless loop in main if Registrar Actor not reachable

src/registrationserver/main.py
@@ -185,20 +185,22 @@ def main():
     while is_flag_set() and retry_counter:
         before = datetime.now()
         with ActorSystem().private() as registrar_status:
-            os_error = True
-            while os_error:
-                try:
-                    reply = registrar_status.ask(
-                        startup_tupel[0],
-                        Thespian_StatusReq(),
-                        timeout=timedelta(seconds=3),
-                    )
-                    os_error = False
-                except OSError as exception:
-                    logger.error("We are offline. %s", exception)
-                    os_error = True
-                    time.sleep(10)
-        if not isinstance(reply, Thespian_ActorStatus):
+            try:
+                reply = registrar_status.ask(
+                    startup_tupel[0],
+                    Thespian_StatusReq(),
+                    timeout=timedelta(seconds=3),
+                )
+            except OSError as exception:
+                logger.critical("We are offline. %s -> Emergency shutdown", exception)
+                set_file_flag(False)
+            except RuntimeError as exception:
+                logger.critical("%s. -> Emergency shutdown", exception)
+                set_file_flag(False)
+            except Exception as exception:  # pylint: disable=broad-except
+                logger.critical("%s. -> Emergency shutdown", exception)
+                set_file_flag(False)
+        if is_flag_set() and not isinstance(reply, Thespian_ActorStatus):
             logger.error("Registrar replied %s instead of Thespian_ActorStatus", reply)
             retry_counter = retry_counter - 1
             if not retry_counter:

commit 094bb8fa7609cc04c5d329b6e1de783927d74742

!: regression from 76f77152d5
http://r2d2.hq.sarad.de:3000/SARAD/RegServer/commit/76f77152d55de0689a715380ff0a8f8767e5c82b

src/registrationserver/main.py
@@ -181,8 +181,7 @@ def main():
     logger.debug("Starting the main loop")
     wait_some_time = False
     registrar_is_down = False
-    retry_counter = 3
-    while is_flag_set() and retry_counter:
+    while is_flag_set():
         before = datetime.now()
         with ActorSystem().private() as registrar_status:
             try:
@@ -194,21 +193,22 @@ def main():
             except OSError as exception:
                 logger.critical("We are offline. %s -> Emergency shutdown", exception)
                 set_file_flag(False)
+                registrar_is_down = True
             except RuntimeError as exception:
                 logger.critical("%s. -> Emergency shutdown", exception)
                 set_file_flag(False)
+                registrar_is_down = True
             except Exception as exception:  # pylint: disable=broad-except
                 logger.critical("%s. -> Emergency shutdown", exception)
                 set_file_flag(False)
+                registrar_is_down = True
         if is_flag_set() and not isinstance(reply, Thespian_ActorStatus):
             logger.error("Registrar replied %s instead of Thespian_ActorStatus", reply)
-            retry_counter = retry_counter - 1
-            if not retry_counter:
-                set_file_flag(False)
-                registrar_is_down = True
-                logger.critical(
-                    "No status response from Registrar Actor. Emergency shutdown."
-                )
+            logger.critical(
+                "No status response from Registrar Actor. Emergency shutdown."
+            )
+            set_file_flag(False)
+            registrar_is_down = True
         time.sleep(1)
         after = datetime.now()
         if (after - before).total_seconds() > 10: