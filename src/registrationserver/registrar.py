SARAD-GmbH/RegServer

commit ec63c64ef1a9daea94b39fad6feabf88452306bf

!: Don't create MQTT or mDNS device actor if local device actor...
already exists.

src/registrationserver/registrar.py
@@ -24,7 +24,8 @@ from registrationserver.actor_messages import (ActorCreatedMsg, Backend,
                                                ReturnDeviceActorMsg,
                                                UpdateActorDictMsg)
 from registrationserver.base_actor import BaseActor
-from registrationserver.helpers import short_id, transport_technology
+from registrationserver.helpers import (is_device_actor, short_id,
+                                        transport_technology)
 from registrationserver.logger import logger
 from registrationserver.modules.backend.is1.is1_listener import Is1Listener
 from registrationserver.modules.backend.mqtt.mqtt_listener import MqttListener
@@ -155,11 +156,11 @@ class Registrar(BaseActor):
         """Handler for UnsubscribeMsg from any actor."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
         try:
-            is_device_actor = self.actor_dict[msg.actor_id]["is_device_actor"]
+            is_device_act = self.actor_dict[msg.actor_id]["is_device_actor"]
         except KeyError:
             logger.error("%s requested to be unsubscribed doesn't exist", msg.actor_id)
-            is_device_actor = False
-        if is_device_actor:
+            is_device_act = False
+        if is_device_act:
             instr_id = short_id(msg.actor_id)
             logger.debug("Look for inactive device actor for %s", instr_id)
             for actor_id, description in self.actor_dict.items():
@@ -204,8 +205,28 @@ class Registrar(BaseActor):
         """Handler for CreateActorMsg. Create a new actor."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
         actor_id = msg.actor_id
+        actor_address = None
         if actor_id not in self.actor_dict:
-            actor_address = self._create_actor(msg.actor_type, actor_id)
+            if is_device_actor(actor_id):
+                # Check for existing device actor
+                new_device_id = msg.actor_id
+                for old_device_id in self.actor_dict:
+                    if (
+                        (short_id(old_device_id) == short_id(new_device_id))
+                        and (new_device_id != old_device_id)
+                        and (
+                            transport_technology(old_device_id)
+                            in ["local", "mdns", "mqtt", "is1"]
+                        )
+                    ):
+                        new_tt = transport_technology(new_device_id)
+                        logger.debug("New device_id: %s", new_device_id)
+                        logger.debug("Old device_id: %s", old_device_id)
+                        if new_tt in ["local", "is1"]:
+                            logger.debug("Create new device_actor %s", actor_id)
+                            actor_address = self._create_actor(msg.actor_type, actor_id)
+            else:
+                actor_address = self._create_actor(msg.actor_type, actor_id)
         else:
             actor_address = self.actor_dict[actor_id]["address"]
         self.send(sender, ActorCreatedMsg(actor_address))

src/registrationserver/helpers.py
@@ -147,6 +147,19 @@ def transport_technology(device_id: str) -> str:
     return device_id.split(".", 2)[-1]
 
 
+def is_device_actor(actor_id: str) -> bool:
+    """Check whether the actor is an device actor.
+    Device actor ids are having the structure instr_id.protocoll.tt
+
+    Args:
+        actor_id (str): if it is a device actor, this is the device_id
+
+    Returns:
+        bool: True if the actor_id has the structure of a device_id
+    """
+    return bool(len(actor_id.split(".")) == 3)
+
+
 def find(pattern, path):
     """Find a file matching a given pattern in a given path.
 

src/registrationserver/modules/backend/mdns/mdns_listener.py
@@ -139,7 +139,7 @@ class MdnsListener(ServiceListener):
                     logger.critical("Got message object of unexpected type")
                     logger.critical("-> Stop and shutdown system")
                     system_shutdown()
-                else:
+                elif reply.actor_address is not None:
                     device_actor = reply.actor_address
                     self.update_device_actor(device_actor, device_id, name, info)
 

commit d526e423077936b65590eae572d61219df7ff161

!: regression from commit ec63c64ef1
http://r2d2.hq.sarad.de:3000/SARAD/RegServer/commit/ec63c64ef1a9daea94b39fad6feabf88452306bf

src/registrationserver/registrar.py
@@ -206,9 +206,15 @@ class Registrar(BaseActor):
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
         actor_id = msg.actor_id
         actor_address = None
-        if actor_id not in self.actor_dict:
+        if actor_id in self.actor_dict:
+            logger.debug("%s already exists", actor_id)
+            actor_address = self.actor_dict[actor_id]["address"]
+        else:
             if is_device_actor(actor_id):
-                # Check for existing device actor
+                logger.debug(
+                    "Check for existing device actor with same instr_id=%s",
+                    short_id(actor_id),
+                )
                 new_device_id = msg.actor_id
                 for old_device_id in self.actor_dict:
                     if (
@@ -219,16 +225,23 @@ class Registrar(BaseActor):
                             in ["local", "mdns", "mqtt", "is1"]
                         )
                     ):
+                        logger.debug("Found an actor with the same instr_id")
                         new_tt = transport_technology(new_device_id)
                         logger.debug("New device_id: %s", new_device_id)
                         logger.debug("Old device_id: %s", old_device_id)
                         if new_tt in ["local", "is1"]:
                             logger.debug("Create new device_actor %s", actor_id)
+                            logger.debug(
+                                "Old device_actor might disapear when handling SetupMsg"
+                            )
                             actor_address = self._create_actor(msg.actor_type, actor_id)
+                        else:
+                            logger.debug("Creation of %s skipped", actor_id)
+                    else:
+                        logger.debug("Create new device_actor %s", actor_id)
+                        actor_address = self._create_actor(msg.actor_type, actor_id)
             else:
                 actor_address = self._create_actor(msg.actor_type, actor_id)
-        else:
-            actor_address = self.actor_dict[actor_id]["address"]
         self.send(sender, ActorCreatedMsg(actor_address))
 
     def receiveMsg_GetDeviceActorMsg(self, msg, sender):