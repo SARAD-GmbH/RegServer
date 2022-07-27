647032d905efc1a1f830d64432f5e736daf38f2e
Michael Strey <mstrey@strey.biz>
2022-07-27 10:32:34 +0200

    !: extend firewall rules to allow mDNS

--- src/add-firewall-rule.ps1
+++ src/add-firewall-rule.ps1
@@ -1,13 +1,37 @@
-$firewallRuleName = "SaradRegistrationServer"
+$firewallRuleName1 = "SaradRegistrationServer1"
+$firewallRuleName2 = "SaradRegistrationServer2"
+$firewallRuleName3 = "SaradRegistrationServer3"
 
-write-host "Checking for '$firewallRuleName' firewall rule now...."
-if ($(Get-NetFirewallRule -DisplayName $firewallRuleName))
+write-host "Checking for '$firewallRuleName1' firewall rule now...."
+if ($(Get-NetFirewallRule -DisplayName $firewallRuleName1))
 {
-    write-host "Firewall rule for '$firewallRuleName' already exists, not creating new rule"
+    write-host "Firewall rule for '$firewallRuleName1' already exists, not creating new rule"
 }
 else
 {
-    write-host "Firewall rule for '$firewallRuleName' does not exist, creating new rule now..."
-    New-NetFirewallRule -DisplayName $firewallRuleName -Action Allow -Description "Allow connections to SARAD Registration Server Service" -Direction Inbound -LocalPort 50000-50500,8008 -Protocol TCP -RemoteAddress LocalSubnet -Service SaradRegistrationServer
-    write-host "Firewall rule for '$firewallRuleName' created successfully"
+    write-host "Firewall rule for '$firewallRuleName1' does not exist, creating new rule now..."
+    New-NetFirewallRule -DisplayName $firewallRuleName1 -Action Allow -Description "Allow connections to SARAD Registration Server Service" -Direction Inbound -LocalPort 50000-50500 -Protocol TCP -RemoteAddress LocalSubnet -Service SaradRegistrationServer -Program %ProgramFiles%\SARAD\RegServer-Service\regserver-service.exe
+    write-host "Firewall rule for '$firewallRuleName1' created successfully"
+}
+write-host "Checking for '$firewallRuleName2' firewall rule now...."
+if ($(Get-NetFirewallRule -DisplayName $firewallRuleName2))
+{
+    write-host "Firewall rule for '$firewallRuleName2' already exists, not creating new rule"
+}
+else
+{
+    write-host "Firewall rule for '$firewallRuleName2' does not exist, creating new rule now..."
+    New-NetFirewallRule -DisplayName $firewallRuleName2 -Action Allow -Description "Allow connections to SARAD Registration Server Service" -Direction Inbound -LocalPort 8008 -Protocol TCP -RemoteAddress LocalSubnet
+    write-host "Firewall rule for '$firewallRuleName2' created successfully"
+}
+write-host "Checking for '$firewallRuleName3' firewall rule now...."
+if ($(Get-NetFirewallRule -DisplayName $firewallRuleName3))
+{
+    write-host "Firewall rule for '$firewallRuleName3' already exists, not creating new rule"
+}
+else
+{
+    write-host "Firewall rule for '$firewallRuleName3' does not exist, creating new rule now..."
+    New-NetFirewallRule -DisplayName $firewallRuleName3 -Action Allow -Description "Allow connections to SARAD Registration Server Service" -Direction Inbound -LocalPort 5353 -Protocol UDP -RemoteAddress LocalSubnet
+    write-host "Firewall rule for '$firewallRuleName3' created successfully"
 }
--- src/rm-firewall-rule.ps1
+++ src/rm-firewall-rule.ps1
@@ -1,13 +1,37 @@
-﻿$firewallRuleName = "SaradRegistrationServer"
+﻿$firewallRuleName1 = "SaradRegistrationServer1"
+$firewallRuleName2 = "SaradRegistrationServer2"
+$firewallRuleName3 = "SaradRegistrationServer3"
 
-write-host "Checking for '$firewallRuleName' firewall rule now...."
-if ($(Get-NetFirewallRule -DisplayName $firewallRuleName))
+write-host "Checking for '$firewallRuleName1' firewall rule now...."
+if ($(Get-NetFirewallRule -DisplayName $firewallRuleName1))
 {
-    write-host "Firewall rule for '$firewallRuleName' found, removing it..."
-    Remove-NetFirewallRule -DisplayName $firewallRuleName
-    write-host "Firewall rule for '$firewallRuleName' removed successfully."
+    write-host "Firewall rule for '$firewallRuleName1' found, removing it..."
+    Remove-NetFirewallRule -DisplayName $firewallRuleName1
+    write-host "Firewall rule for '$firewallRuleName1' removed successfully."
 }
 else
 {
-    write-host "Firewall rule for '$firewallRuleName' does not exist, nothing to do."
+    write-host "Firewall rule for '$firewallRuleName1' does not exist, nothing to do."
+}
+write-host "Checking for '$firewallRuleName2' firewall rule now...."
+if ($(Get-NetFirewallRule -DisplayName $firewallRuleName2))
+{
+    write-host "Firewall rule for '$firewallRuleName2' found, removing it..."
+    Remove-NetFirewallRule -DisplayName $firewallRuleName2
+    write-host "Firewall rule for '$firewallRuleName2' removed successfully."
+}
+else
+{
+    write-host "Firewall rule for '$firewallRuleName2' does not exist, nothing to do."
+}
+write-host "Checking for '$firewallRuleName3' firewall rule now...."
+if ($(Get-NetFirewallRule -DisplayName $firewallRuleName3))
+{
+    write-host "Firewall rule for '$firewallRuleName3' found, removing it..."
+    Remove-NetFirewallRule -DisplayName $firewallRuleName3
+    write-host "Firewall rule for '$firewallRuleName3' removed successfully."
+}
+else
+{
+    write-host "Firewall rule for '$firewallRuleName3' does not exist, nothing to do."
 }
