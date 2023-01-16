SARAD-GmbH/RegServer

commit b3fe6f2aaea68210b6f3e4d3b7c47594b28358a4

!: avoid to write stop file multiple times

src/registrationserver/shutdown.py
@@ -26,9 +26,8 @@ def set_file_flag(running, with_error=False):
     Returns:
         None
     """
-    if running:
-        if os.path.isfile(FLAGFILENAME):
-            os.unlink(FLAGFILENAME)
+    if running and os.path.isfile(FLAGFILENAME):
+        os.unlink(FLAGFILENAME)
         logger.info("Remove %s", FLAGFILENAME)
     else:
         with open(FLAGFILENAME, "w", encoding="utf8") as flag_file:

commit 3d8b9e953f28c95228278b332fe4add1a398f676

!: Shutdown after restart of computer #224
Regression from b3fe6f2aae

src/registrationserver/shutdown.py
@@ -22,20 +22,21 @@ def set_file_flag(running, with_error=False):
 
     Args:
         running (bool): If False, the file will be created and the system shall be shutdown.
+        with_error (bool): If True, the system shall be shutdown with error
+                           in order to restart the service automatically.
 
     Returns:
         None
     """
-    if running:
-        try:
-            os.remove(FLAGFILENAME)
-            logger.info("Remove %s", FLAGFILENAME)
-        except FileNotFoundError:
-            logger.info("%s not found", FLAGFILENAME)
-    else:
+    try:
+        os.remove(FLAGFILENAME)
+        logger.info("Remove %s", FLAGFILENAME)
+    except FileNotFoundError:
+        logger.info("%s not found", FLAGFILENAME)
+    if not running:
         with open(FLAGFILENAME, "w", encoding="utf8") as flag_file:
             flag_file.write(str(with_error))
-        logger.info("Write %s", FLAGFILENAME)
+        logger.info("Write %s, with_error = %s", FLAGFILENAME, with_error)
 
 
 def is_flag_set():