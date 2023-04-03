SARAD-GmbH/RegServer

commit 72aceee43cb5e649413c9784a69a0bf426aac4e1

!: ROOMS doesn't work with #254

src/registrationserver/modules/device_actor.py
@@ -155,6 +155,21 @@ class DeviceBaseActor(BaseActor):
             system_shutdown()
         self._send_reservation_status_msg()
 
+    def _handle_free_reply_from_is(self, success: Status):
+        # pylint: disable=unused-argument
+        """Inform all interested parties that the instrument is free.
+        Forward the reservation state from the Instrument Server to the REST API.
+        This function has to be called in the protocol specific modules.
+        """
+        logger.debug("Free command returned %s", success)
+        self._forward_to_children(KillMsg())
+        self.return_message = ReservationStatusMsg(self.instr_id, success)
+        if success == Status.OK:
+            self._publish_status_change()
+        if not self.child_actors:
+            self._send_reservation_status_msg()
+        logger.info("Free %s", self.my_id)
+
     def _create_redirector(self) -> bool:
         """Create redirector actor if it does not exist already"""
         if not self.child_actors:
@@ -208,31 +223,6 @@ class DeviceBaseActor(BaseActor):
             return
         self.return_message = ReservationStatusMsg(self.instr_id, Status.FREE_PENDING)
         self.sender_api = sender
-        status = Status.OK
-        try:
-            if self.device_status["Reservation"]["Active"]:
-                logger.info("Free active %s", self.my_id)
-                self.device_status["Reservation"]["Active"] = False
-                if self.device_status["Reservation"].get("IP") is not None:
-                    self.device_status["Reservation"].pop("IP")
-                if self.device_status["Reservation"].get("Port") is not None:
-                    self.device_status["Reservation"].pop("Port")
-                self.device_status["Reservation"]["Timestamp"] = (
-                    datetime.utcnow().isoformat(timespec="seconds") + "Z"
-                )
-                status = Status.OK
-            else:
-                status = Status.OK_SKIPPED
-        except KeyError:
-            logger.debug("Instr. was not reserved before.")
-            status = Status.OK_SKIPPED
-        self._forward_to_children(KillMsg())
-        self.return_message = ReservationStatusMsg(self.instr_id, status)
-        if status == Status.OK:
-            self._publish_status_change()
-        if not self.child_actors:
-            self._send_reservation_status_msg()
-        logger.info("Free %s", self.my_id)
 
     def receiveMsg_GetDeviceStatusMsg(self, msg, sender):
         # pylint: disable=invalid-name

src/registrationserver/modules/backend/mdns/device_actor.py
@@ -172,6 +172,26 @@ class DeviceActor(DeviceBaseActor):
                 success = Status.OK_SKIPPED
         logger.debug("Freeing remote device ended with %s", success)
         super().receiveMsg_FreeDeviceMsg(msg, sender)
+        try:
+            if self.device_status["Reservation"]["Active"] and (
+                status in (Status.OK, Status.OK_SKIPPED)
+            ):
+                logger.info("Free active %s", self.my_id)
+                self.device_status["Reservation"]["Active"] = False
+                if self.device_status["Reservation"].get("IP") is not None:
+                    self.device_status["Reservation"].pop("IP")
+                if self.device_status["Reservation"].get("Port") is not None:
+                    self.device_status["Reservation"].pop("Port")
+                self.device_status["Reservation"]["Timestamp"] = (
+                    datetime.utcnow().isoformat(timespec="seconds") + "Z"
+                )
+                status = Status.OK
+            else:
+                status = Status.OK_SKIPPED
+        except KeyError:
+            logger.debug("Instr. was not reserved before.")
+            status = Status.OK_SKIPPED
+        self._handle_free_reply_from_is(success)
 
     @overrides
     def _request_reserve_at_is(self):

src/registrationserver/modules/backend/is1/is1_actor.py
@@ -194,6 +194,26 @@ class Is1Actor(DeviceBaseActor):
     def receiveMsg_FreeDeviceMsg(self, msg, sender):
         self._destroy_socket()
         super().receiveMsg_FreeDeviceMsg(msg, sender)
+        try:
+            if self.device_status["Reservation"]["Active"] and (
+                status in (Status.OK, Status.OK_SKIPPED)
+            ):
+                logger.info("Free active %s", self.my_id)
+                self.device_status["Reservation"]["Active"] = False
+                if self.device_status["Reservation"].get("IP") is not None:
+                    self.device_status["Reservation"].pop("IP")
+                if self.device_status["Reservation"].get("Port") is not None:
+                    self.device_status["Reservation"].pop("Port")
+                self.device_status["Reservation"]["Timestamp"] = (
+                    datetime.utcnow().isoformat(timespec="seconds") + "Z"
+                )
+                status = Status.OK
+            else:
+                status = Status.OK_SKIPPED
+        except KeyError:
+            logger.debug("Instr. was not reserved before.")
+            status = Status.OK_SKIPPED
+        self._handle_free_reply_from_is(Status.OK)
 
     @overrides
     def receiveMsg_KillMsg(self, msg, sender):

src/registrationserver/modules/backend/mqtt/mqtt_actor.py
@@ -230,6 +230,14 @@ class MqttActor(DeviceBaseActor, MqttBaseActor):
             )  # create redirector actor
             self.state["RESERVE"]["Pending"] = False
             return
+        if (
+            (not reservation.get("Active", False))
+            and (self.return_message is not None)
+            and (self.return_message.status is not None)
+            and (self.return_message.status == Status.FREE_PENDING)
+        ):
+            self._handle_free_reply_from_is(Status.OK)
+            return
         logger.warning(
             "MQTT actor received a reply to a non-requested reservation on instrument %s",
             self.my_id,

src/registrationserver/modules/frontend/mqtt/mqtt_scheduler.py
@@ -159,7 +159,7 @@ class MqttSchedulerActor(MqttBaseActor):
                     logger.debug(
                         "[FREE] Instrument is not in the list of reserved instruments."
                     )
-                    return
+                    reservation_object.status = Status.OK_SKIPPED
                 logger.debug("Publish free status")
                 if reservation_object.status in (
                     Status.OK,

commit 0d7f2008db1ccb0b9f2af8ab6d71636e28503f8c

!: regression from 72aceee43c

src/registrationserver/modules/device_actor.py
@@ -181,6 +181,23 @@ class DeviceBaseActor(BaseActor):
         This function has to be called in the protocol specific modules.
         """
         logger.debug("Free command returned %s", success)
+        if success in (Status.OK, Status.OK_SKIPPED, Status.OK_UPDATED):
+            try:
+                if self.device_status["Reservation"]["Active"]:
+                    logger.info("Free active %s", self.my_id)
+                    self.device_status["Reservation"]["Active"] = False
+                    if self.device_status["Reservation"].get("IP") is not None:
+                        self.device_status["Reservation"].pop("IP")
+                    if self.device_status["Reservation"].get("Port") is not None:
+                        self.device_status["Reservation"].pop("Port")
+                    self.device_status["Reservation"]["Timestamp"] = (
+                        datetime.utcnow().isoformat(timespec="seconds") + "Z"
+                    )
+                else:
+                    success = Status.OK_SKIPPED
+            except KeyError:
+                logger.debug("Instr. was not reserved before.")
+                success = Status.OK_SKIPPED
         self._forward_to_children(KillMsg())
         self.return_message = ReservationStatusMsg(self.instr_id, success)
         if success == Status.OK: