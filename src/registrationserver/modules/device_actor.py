SARAD-GmbH/RegServer

commit c3890f56777bfa6ccb864db555545e51e0e6a44e

!: No response from Actor System due to concurrent access #222

src/registrationserver/modules/device_actor.py
@@ -82,6 +82,11 @@ class DeviceBaseActor(BaseActor):
         # pylint: disable=invalid-name
         """Handler for ReserveDeviceMsg from REST API."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
+        if (self.return_message is not None) and (
+            self.return_message.status == Status.FREE_PENDING
+        ):
+            self._send_reservation_status_msg()
+            return
         self.return_message = ReservationStatusMsg(
             self.instr_id, Status.RESERVE_PENDING
         )
@@ -196,6 +201,11 @@ class DeviceBaseActor(BaseActor):
         # pylint: disable=invalid-name
         """Handler for FreeDeviceMsg from REST API."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
+        if (self.return_message is not None) and (
+            self.return_message.status == Status.FREE_PENDING
+        ):
+            self._send_reservation_status_msg()
+            return
         self.return_message = ReservationStatusMsg(self.instr_id, Status.FREE_PENDING)
         self.sender_api = sender
         status = Status.OK

commit bfdb2015da808d179b5bb7e32164ac303c444e89

!: bug introduced in c3890f5677
FREE request returned Status.FREE_PENDING

src/registrationserver/modules/device_actor.py
@@ -169,7 +169,7 @@ class DeviceBaseActor(BaseActor):
         """Handler for FreeDeviceMsg from REST API."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
         if (self.return_message is not None) and (
-            self.return_message.status == Status.FREE_PENDING
+            self.return_message.status == Status.RESERVE_PENDING
         ):
             self._send_reservation_status_msg()
             return