37acad0027e37af182a664031b4a37f64790bc87
Michael Strey <mstrey@strey.biz>
2023-04-17 16:02:48 +0200

    +: introduce threading in IS1 Actor
    not yet complete

--- src/registrationserver/modules/backend/is1/is1_actor.py
+++ src/registrationserver/modules/backend/is1/is1_actor.py
@@ -8,8 +8,10 @@ as used in Instrument Server 1
     | Michael Strey <strey@sarad.de>
 """
 import socket
-import time
-from datetime import datetime, timedelta
+from datetime import timedelta
+from enum import Enum
+from threading import Thread
+from time import sleep
 
 from overrides import overrides  # type: ignore
 from registrationserver.actor_messages import (Is1Address, Is1RemoveMsg,
@@ -39,7 +41,15 @@ class Is1Actor(DeviceBaseActor):
         self._is: Is1Address = None
         self._com_port = None
         self._socket = None
-        self.last_activity = datetime.utcnow()
+        self.check_connection_thread = Thread(
+            target=self.scan_is,
+            daemon=True,
+        )
+        self.tx_binary_thread = Thread(
+            target=self._tx_binary,
+            kwargs={"data": None, "sender": None},
+            daemon=True,
+        )
 
     def receiveMsg_SetupIs1ActorMsg(self, msg, sender):
         # pylint: disable=invalid-name
@@ -53,49 +63,50 @@ class Is1Actor(DeviceBaseActor):
     def receiveMsg_WakeupMessage(self, msg, _sender):
         # pylint: disable=invalid-name
         """Handler for WakeupMessage"""
-        if msg.payload == "Rescan" and not self.on_kill:
-            time_condition = bool(
-                datetime.utcnow() - self.last_activity > timedelta(seconds=10)
-            )
+        try:
+            is_reserved = self.device_status["Reservation"]["Active"]
+        except KeyError:
+            is_reserved = False
+        if (msg.payload == "Rescan") and (not self.on_kill) and (not is_reserved):
             logger.debug(
-                "time_condition = %s",
-                time_condition,
+                "Check %s for living instruments",
+                self._is.hostname,
             )
-            if time_condition:
-                logger.debug(
-                    "Check %s for living instruments",
-                    self._is.hostname,
-                )
-                self._scan_is(self._is)
-            self.wakeupAfter(timedelta(seconds=60), payload="Rescan")
-            self.last_activity = datetime.utcnow()
+            self.scan_is(self._is)
 
     def _establish_socket(self):
-        if self._socket is None:
-            socket.setdefaulttimeout(5)
-            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            retry_counter = 2
-            while retry_counter:
-                try:
-                    logger.debug(
-                        "Trying to connect %s:%d", self._is.ip_address, self._is.port
-                    )
-                    self._socket.connect((self._is.hostname, self._is.port))
-                    retry_counter = 0
-                    return True
-                except ConnectionRefusedError:
-                    retry_counter = retry_counter - 1
-                    logger.debug("Connection refused. %d retries left", retry_counter)
-                    time.sleep(1)
-                except (TimeoutError, socket.timeout, ConnectionResetError):
-                    logger.error("Timeout connecting %s", self._is.hostname)
-                    retry_counter = 0
-                except BlockingIOError:
-                    logger.error("BlockingIOError connecting %s", self._is.hostname)
-                    retry_counter = 0
-            self.send(self.myAddress, KillMsg())
-            return False
-        return True
+        try:
+            if self._socket is None:
+                socket.setdefaulttimeout(5)
+                self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                retry_counter = 2
+                while retry_counter:
+                    try:
+                        logger.debug(
+                            "Trying to connect %s:%d",
+                            self._is.ip_address,
+                            self._is.port,
+                        )
+                        self._socket.connect((self._is.hostname, self._is.port))
+                        retry_counter = 0
+                        return
+                    except ConnectionRefusedError:
+                        retry_counter = retry_counter - 1
+                        logger.debug(
+                            "Connection refused. %d retries left", retry_counter
+                        )
+                        sleep(1)
+                    except (TimeoutError, socket.timeout, ConnectionResetError):
+                        logger.error("Timeout connecting %s", self._is.hostname)
+                        retry_counter = 0
+                    except BlockingIOError:
+                        logger.error("BlockingIOError connecting %s", self._is.hostname)
+                        retry_counter = 0
+                self.send(self.myAddress, KillMsg())
+                self._socket = None
+        except OSError as re_exception:
+            logger.error("Failed to re-establish socket: %s", re_exception)
+            self._socket = None
 
     def _destroy_socket(self):
         if self._socket is not None:
@@ -117,13 +128,10 @@ class Is1Actor(DeviceBaseActor):
                 success = True
             except OSError as exception:
                 logger.error(exception)
-                try:
-                    self._establish_socket()
-                except OSError as re_exception:
-                    logger.error("Failed to re-establish socket: %s", re_exception)
+                self._establish_socket()
                 retry_counter = retry_counter - 1
                 logger.debug("%d retries left", retry_counter)
-                time.sleep(1)
+                sleep(1)
         return success
 
     @overrides
@@ -131,7 +139,22 @@ class Is1Actor(DeviceBaseActor):
         # pylint: disable=invalid-name
         """Handler for TxBinaryMsg from App to Instrument."""
         super().receiveMsg_TxBinaryMsg(msg, sender)
-        if not self._establish_socket():
+        if (
+            not self.check_connection_thread.is_alive()
+            and not self.tx_binary_thread.is_alive()
+        ):
+            self.tx_binary_thread = Thread(
+                target=self._tx_binary,
+                kwargs={"data": msg.data},
+                daemon=True,
+            )
+            self.tx_binary_thread.start()
+        else:
+            logger.critical("Conflict between threads in TxBinaryMsg handler")
+
+    def _tx_binary(self, data):
+        self._establish_socket()
+        if self._socket is None:
             logger.error("Can't establish the client socket.")
             return
         # Dirty workaround for bug in SARAD instruments
@@ -139,7 +162,7 @@ class Is1Actor(DeviceBaseActor):
         retry_counter = 2
         success = False
         while retry_counter:
-            if self._send_via_socket(msg.data):
+            if self._send_via_socket(data):
                 try:
                     reply = self._socket.recv(1024)
                     retry_counter = 0
@@ -155,39 +178,48 @@ class Is1Actor(DeviceBaseActor):
             self.send(self.myAddress, KillMsg())
             reply = b""
         self.send(self.redirector_actor, RxBinaryMsg(reply))
-        self.last_activity = datetime.utcnow()
 
     @overrides
     def _request_reserve_at_is(self):
         # pylint: disable=unused-argument
         """Reserve the requested instrument at the instrument server."""
-        if not self._establish_socket():
-            logger.error("Can't establish the client socket.")
-            self._handle_reserve_reply_from_is(Status.IS_NOT_FOUND)
-            return
-        cmd_msg = make_command_msg(
-            [self.SELECT_COM, (self._com_port).to_bytes(1, byteorder="little")]
-        )
-        if self._send_via_socket(cmd_msg):
-            try:
-                reply = self._socket.recv(1024)
-            except (TimeoutError, socket.timeout, ConnectionResetError):
-                logger.error("Timeout on waiting for reply to SELECT_COM: %s", cmd_msg)
-                status = Status.IS_NOT_FOUND
-            else:
-                checked_reply = check_message(reply, multiframe=False)
-                logger.debug("Reserve at IS1 replied %s", checked_reply)
-                if (
-                    checked_reply["is_valid"]
-                    and checked_reply["payload"][0].to_bytes(1, byteorder="little")
-                    == self.COM_SELECTED
-                ):
-                    status = Status.OK
-                else:
-                    status = Status.NOT_FOUND
+        try:
+            is_reserved = self.device_status["Reservation"]["Active"]
+        except KeyError:
+            is_reserved = False
+        if is_reserved:
+            status = Status.OCCUPIED
         else:
-            status = Status.IS_NOT_FOUND
-        self._destroy_socket()
+            self._establish_socket()
+            if self._socket is None:
+                logger.error("Can't establish the client socket.")
+                self._handle_reserve_reply_from_is(Status.IS_NOT_FOUND)
+                return
+            cmd_msg = make_command_msg(
+                [self.SELECT_COM, (self._com_port).to_bytes(1, byteorder="little")]
+            )
+            if self._send_via_socket(cmd_msg):
+                try:
+                    reply = self._socket.recv(1024)
+                except (TimeoutError, socket.timeout, ConnectionResetError):
+                    logger.error(
+                        "Timeout on waiting for reply to SELECT_COM: %s", cmd_msg
+                    )
+                    status = Status.IS_NOT_FOUND
+                else:
+                    checked_reply = check_message(reply, multiframe=False)
+                    logger.debug("Reserve at IS1 replied %s", checked_reply)
+                    if (
+                        checked_reply["is_valid"]
+                        and checked_reply["payload"][0].to_bytes(1, byteorder="little")
+                        == self.COM_SELECTED
+                    ):
+                        status = Status.OK
+                    else:
+                        status = Status.NOT_FOUND
+            else:
+                status = Status.IS_NOT_FOUND
+            self._destroy_socket()
         self._handle_reserve_reply_from_is(status)
 
     @overrides
@@ -201,11 +233,12 @@ class Is1Actor(DeviceBaseActor):
         self.send(self.parent.parent_address, Is1RemoveMsg(is1_address=self._is))
         super().receiveMsg_KillMsg(msg, sender)
 
-    def _scan_is(self, is1_address: Is1Address):
+    def scan_is(self, is1_address: Is1Address):
+        """Look for SARAD instruments at the given Instrument Server"""
         is_host = is1_address.hostname
         is_port = is1_address.port
         cmd_msg = make_command_msg(self.GET_FIRST_COM)
-        logger.debug("Send GetFirstCOM: %s", cmd_msg)
+        logger.info("Send GetFirstCOM: %s", cmd_msg)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             client_socket.settimeout(3)
             retry = True
@@ -218,7 +251,7 @@ class Is1Actor(DeviceBaseActor):
                 except ConnectionRefusedError:
                     counter = counter - 1
                     logger.debug("%d retries left", counter)
-                    time.sleep(1)
+                    sleep(1)
                 except (OSError, TimeoutError, socket.timeout):
                     logger.debug("%s:%d not reachable", is_host, is_port)
                     self.send(self.myAddress, KillMsg())
@@ -253,4 +286,4 @@ class Is1Actor(DeviceBaseActor):
                     return
                 checked_reply = check_message(reply, multiframe=False)
             client_socket.shutdown(socket.SHUT_WR)
-            return
+        self.wakeupAfter(timedelta(seconds=60), payload="Rescan")
