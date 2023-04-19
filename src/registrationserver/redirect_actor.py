092565206ed8591515c557ac16481e784bbbd908
Michael Strey <mstrey@strey.biz>
2023-04-19 10:24:02 +0200

    ~: introduce threading in Redirector Actor

--- src/registrationserver/redirect_actor.py
+++ src/registrationserver/redirect_actor.py
@@ -11,10 +11,10 @@ socket as actor messages to the device actor.
 .. uml :: uml-redirect_actor.puml
 
 """
-import datetime
 import select
 import socket
 import time
+from threading import Thread
 
 from overrides import overrides  # type: ignore
 
@@ -24,8 +24,6 @@ from registrationserver.base_actor import BaseActor
 from registrationserver.config import config, rest_frontend_config
 from registrationserver.logger import logger
 
-# logger.debug("%s -> %s", __package__, __file__)
-
 
 class RedirectorActor(BaseActor):
     """Create listening server socket for binary pakets from a SARAD© Application"""
@@ -34,63 +32,62 @@ class RedirectorActor(BaseActor):
     def __init__(self):
         super().__init__()
         self.my_parent = None
-        self._client_socket = None
-        self._socket_info = None
         self.conn = None
-        self._host = config["MY_IP"]
-        self._port = None
         self.read_list = None
-        logger.info("Redirector Actor initialized")
-
-    def receiveMsg_WakeupMessage(self, msg, _sender):
-        # pylint: disable=invalid-name
-        """Handler for WakeupMessage"""
-        if msg.payload == "loop":
-            self._loop()
+        self.socket_loop_thread = Thread(
+            target=self._loop,
+            daemon=True,
+        )
+        self.send_thread = Thread(
+            target=self._sendall,
+            kwargs={"data": None},
+            daemon=True,
+        )
+        logger.debug("Redirector Actor initialized")
 
     def _loop(self):
-        if not self.on_kill:
-            # Listen to socket and redirect any message from the socket to the device actor
-            server_socket = self.read_list[0]
-            timeout = 0.1
-            readable, _writable, _errored = select.select(
-                self.read_list, [], [], timeout
-            )
-            for self.conn in readable:
-                if self.conn is server_socket:
-                    self._client_socket, self._socket_info = server_socket.accept()
-                    self.read_list.append(self._client_socket)
-                    logger.debug("Connection from %s", self._socket_info)
-                else:
-                    self._cmd_handler()
-            self.wakeupAfter(datetime.timedelta(seconds=0.055), payload="loop")
+        while self.read_list:
+            if not self.on_kill:
+                # Listen to socket and redirect any message from the socket to the device actor
+                server_socket = self.read_list[0]
+                timeout = 0.1
+                readable, _writable, _errored = select.select(
+                    self.read_list, [], [], timeout
+                )
+                for self.conn in readable:
+                    if self.conn is server_socket:
+                        client_socket, socket_info = server_socket.accept()
+                        self.read_list.append(client_socket)
+                        logger.debug("Connection from %s", socket_info)
+                    else:
+                        self._cmd_handler()
 
     @overrides
     def receiveMsg_SetupMsg(self, msg, sender):
         logger.debug("Setup redirector actor")
         super().receiveMsg_SetupMsg(msg, sender)
-        for self._port in rest_frontend_config["PORT_RANGE"]:
+        for port in rest_frontend_config["PORT_RANGE"]:
             try:
                 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server_socket.bind((self._host, self._port))
-                self._port = server_socket.getsockname()[1]
+                server_socket.bind((config["MY_IP"], port))
+                port = server_socket.getsockname()[1]
                 break
             except OSError:
                 try:
                     server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-                    server_socket.bind((self._host, self._port))
-                    self._port = server_socket.getsockname()[1]
+                    server_socket.bind((config["MY_IP"], port))
+                    port = server_socket.getsockname()[1]
                     break
                 except OSError as exception:
-                    logger.error("Cannot use port %d. %s", self._port, exception)
+                    logger.error("Cannot use port %d. %s", port, exception)
                     server_socket.close()
         try:
             server_socket.listen()  # listen(5) maybe???
         except OSError:
-            self._port = None
+            port = None
         logger.debug("Server socket: %s", server_socket)
         self.read_list = [server_socket]
-        if self._port is None:
+        if port is None:
             logger.critical(
                 "Cannot open socket in the configured port range %s",
                 rest_frontend_config["PORT_RANGE"],
@@ -99,17 +96,17 @@ class RedirectorActor(BaseActor):
         elif self.my_parent is None:
             self.my_parent = sender
             return_msg = SocketMsg(
-                ip_address=self._host, port=self._port, status=Status.OK
+                ip_address=config["MY_IP"], port=port, status=Status.OK
             )
         else:
             logger.debug("my_parent: %s", self.my_parent)
             return_msg = SocketMsg(
-                ip_address=self._host, port=self._port, status=Status.OK_SKIPPED
+                ip_address=config["MY_IP"], port=port, status=Status.OK_SKIPPED
             )
         logger.debug("Setup finished with %s", return_msg)
         self.send(sender, return_msg)
         logger.debug("Start socket loop")
-        self.wakeupAfter(datetime.timedelta(seconds=0.01), payload="loop")
+        self.socket_loop_thread.start()
 
     @overrides
     def receiveMsg_KillMsg(self, msg, sender):
@@ -135,9 +132,8 @@ class RedirectorActor(BaseActor):
             self.send(self.myAddress, KillMsg())
         else:
             logger.debug(
-                "Redirect %s from app, socket %s to device actor %s",
+                "Redirect %s from app to device actor %s",
                 data,
-                self._socket_info,
                 self.my_parent,
             )
             self.send(self.my_parent, TxBinaryMsg(data))
@@ -146,9 +142,17 @@ class RedirectorActor(BaseActor):
         # pylint: disable=invalid-name
         """Redirect any received reply to the socket."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
+        self.send_thread = Thread(
+            target=self._sendall,
+            kwargs={"data": msg.data},
+            daemon=True,
+        )
+        self.send_thread.start()
+
+    def _sendall(self, data):
         for _i in range(0, 5):
             try:
-                self.conn.sendall(msg.data)
+                self.conn.sendall(data)
                 return
             except (ConnectionResetError, BrokenPipeError):
                 logger.error("Connection reset by SARAD application software.")
