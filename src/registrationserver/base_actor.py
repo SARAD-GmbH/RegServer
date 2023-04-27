SARAD-GmbH/RegServer

commit 0e34226b03b9741a8b480f6ac03440c968e4b7a5

~: Improve logging hunting Reverse/Free confusion
~: Avoid unneeded ActorExitRequest() messages

src/registrationserver/base_actor.py
@@ -113,7 +113,6 @@ class BaseActor(ActorTypeDispatcher):
                 self._unsubscribe_from_actor_dict_msg()
             if self.child_actors:
                 self._forward_to_children(msg)
-                self._forward_to_children(ActorExitRequest())
             else:
                 if msg.register:
                     self.send(self.registrar, UnsubscribeMsg(actor_id=self.my_id))
@@ -159,16 +158,18 @@ class BaseActor(ActorTypeDispatcher):
         """Handler for ChildActorExited"""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
         actor_id = self._get_actor_id(msg.childAddress, self.child_actors)
-        self.send(self.registrar, UnsubscribeMsg(actor_id))
-        self.child_actors.pop(actor_id, None)
-        logger.debug(
-            "List of child actors after removal of %s: %s", actor_id, self.child_actors
-        )
+        child_actor = self.child_actors.pop(actor_id, None)
+        if child_actor is not None:
+            # self.send(self.registrar, UnsubscribeMsg(actor_id))
+            logger.debug(
+                "List of child actors after removal of %s: %s",
+                actor_id,
+                self.child_actors,
+            )
         logger.debug("self.on_kill is %s", self.on_kill)
         if (not self.child_actors) and self.on_kill:
-            logger.debug("Unsubscribe and send ActorExitRequest to myself")
+            logger.debug("Unsubscribe from Registrar")
             self.send(self.registrar, UnsubscribeMsg(actor_id=self.my_id))
-            self.send(self.myAddress, ActorExitRequest())
 
     def receiveMsg_ActorExitRequest(self, msg, sender):
         # pylint: disable=invalid-name

src/registrationserver/helpers.py
@@ -296,6 +296,7 @@ def get_device_status(registrar_actor, device_id: str) -> dict:
         )
         system_shutdown()
         return {}
+    logger.debug("get_device_status() returned with %s", result.device_status)
     return result.device_status
 
 
@@ -448,6 +449,7 @@ def send_free_message(device_id, registrar_actor) -> Status:
 
     Returns: Success status
     """
+    logger.debug("Before FREE operation")
     device_state = get_device_status(registrar_actor, device_id)
     if (device_state == {}) or (
         device_actor := get_actor(registrar_actor, device_id)

src/registrationserver/modules/backend/mqtt/mqtt_actor.py
@@ -190,6 +190,7 @@ class MqttActor(DeviceBaseActor, MqttBaseActor):
         """Handler for MQTT messages regarding reservation of instruments"""
         reservation_status = Status.ERROR
         reservation = json.loads(message.payload)
+        logger.debug("[on_reserve] received: %s", reservation)
         if self.state["RESERVE"]["Pending"]:
             instr_status = reservation.get("Active")
             app = reservation.get("App")

src/registrationserver/restapi.py
@@ -243,6 +243,7 @@ class ListDevice(Resource):
 
     def get(self, device_id):
         """Get indentifying information for a single available instrument."""
+        logger.debug("ListDevice %s", device_id)
         if (registrar_actor := get_registrar_actor()) is None:
             status = Status.CRITICAL
             logger.critical("No response from Actor System. -> Emergency shutdown")
@@ -275,6 +276,7 @@ class ReserveDevice(Resource):
     def get(self, device_id):
         """Reserve an available instrument so that nobody else can use it."""
         # Collect information about who sent the request.
+        logger.debug("ReserveDevice %s", device_id)
         if (registrar_actor := get_registrar_actor()) is None:
             status = Status.CRITICAL
             logger.critical("No response from Registrar Actor. -> Emergency shutdown")
@@ -305,6 +307,7 @@ class ReserveDevice(Resource):
             attribute_who,
             request_host,
         )
+        logger.debug("Before RESERVE operation")
         device_state = get_device_status(registrar_actor, device_id)
         if (
             transport_technology(device_id) not in ["local", "is1", "mdns", "mqtt"]
@@ -323,6 +326,7 @@ class ReserveDevice(Resource):
             Status.OK_UPDATED,
             Status.OCCUPIED,
         ):
+            logger.debug("After RESERVE operation")
             return {
                 "Error code": status.value,
                 "Error": str(status),
@@ -359,6 +363,7 @@ class FreeDevice(Resource):
 
     def get(self, device_id):
         """Free/release a device that was reserved before"""
+        logger.debug("FreeDevice %s", device_id)
         if (registrar_actor := get_registrar_actor()) is None:
             status = Status.CRITICAL
         else:
@@ -382,6 +387,7 @@ class FreeDevice(Resource):
             Status.OK_UPDATED,
             Status.OCCUPIED,
         ):
+            logger.debug("After FREE operation")
             return {
                 "Error code": status.value,
                 "Error": str(status),

commit 70ab452991a6f606e43a51161e91f61b65132055

!: Regression from 0e34226b03

src/registrationserver/base_actor.py
@@ -117,12 +117,6 @@ class BaseActor(ActorTypeDispatcher):
                 if msg.register:
                     self.send(self.registrar, UnsubscribeMsg(actor_id=self.my_id))
                 self.send(self.myAddress, ActorExitRequest())
-                if self.is_device_actor:
-                    logger.info(
-                        "Device actor %s exited at %s.",
-                        self.my_id,
-                        self.parent.parent_id,
-                    )
 
     def receiveMsg_KeepAliveMsg(self, msg, sender):
         # pylint: disable=invalid-name, unused-argument
@@ -168,13 +162,22 @@ class BaseActor(ActorTypeDispatcher):
             )
         logger.debug("self.on_kill is %s", self.on_kill)
         if (not self.child_actors) and self.on_kill:
-            logger.debug("Unsubscribe from Registrar")
+            logger.debug(
+                "Unsubscribe from Registrar and send ActorExitRequest to myself"
+            )
             self.send(self.registrar, UnsubscribeMsg(actor_id=self.my_id))
+            self.send(self.myAddress, ActorExitRequest())
 
     def receiveMsg_ActorExitRequest(self, msg, sender):
         # pylint: disable=invalid-name
         """Handler for ActorExitRequest"""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
+        if self.is_device_actor:
+            logger.info(
+                "Device actor %s exited at %s.",
+                self.my_id,
+                self.parent.parent_id,
+            )
         # self.send(self.registrar, UnsubscribeMsg(actor_id=self.my_id))
 
     def receiveMsg_DeadEnvelope(self, msg, sender):