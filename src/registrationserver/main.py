SARAD-GmbH/RegServer

commit d566e876c17e46cd4cafbd6ac41cf12ea5ad35e6

~: improve ERROR log

src/registrationserver/main.py
@@ -237,7 +237,8 @@ def outer_watchdog(registrar_address, number_of_trials=0) -> bool:
        True if the Registrar is alive.
     """
     registrar_is_down = False
-    while number_of_trials:
+    attempts_left = number_of_trials
+    while attempts_left:
         logger.debug("Run outer watchdog")
         registrar_is_down = False
         with ActorSystem().private() as registrar_status:

commit 008d0234c38a85a81ae23973060327e35a2b129f

!: regression introduced in d566e876c1

src/registrationserver/main.py
@@ -259,11 +259,11 @@ def outer_watchdog(registrar_address, number_of_trials=0) -> bool:
                 logger.critical("Exception: %s.", exception)
                 registrar_is_down = True
         if registrar_is_down:
-            number_of_trials = 0  # don't retry, stop it!
+            attempts_left = 0  # don't retry, stop it!
         else:
             if isinstance(reply, Thespian_ActorStatus):
                 # logger.debug("Aye Sir!")
-                number_of_trials = 0
+                attempts_left = 0
                 registrar_is_down = False
             else:
                 attempts_left = attempts_left - 1

src/registrationserver/modules/backend/usb/com_actor.py
@@ -55,7 +55,6 @@ class ComActor(BaseActor):
             # refer to receiveMsg_ChildActorExited()
         else:
             logger.debug("%s: Update -- no child", self.my_id)
-            self.instrument = None
             self._do_loop()
             self._start_polling()
 