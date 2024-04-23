SARAD-GmbH/RegServer

commit eedaad8a4c74923be953338d0ceb3e9a2acfd86a

!: handle seldom case of mixed up ZigBee addresses

src/regserver/modules/backend/usb/net_usb_actor.py
@@ -129,11 +129,37 @@ class NetUsbActor(UsbActor):
         new_channels = {}
         for instr_id, address in channels.items():
             actor_id = self.get_actor_id(instr_id)
+            if self.child_actors:
+                old_zb_address = self.child_actors[actor_id][
+                    "route_to_instr"
+                ].zigbee_address
+            else:
+                old_zb_address = 0
+            new_zb_address = channels[short_id(actor_id)]
             if actor_id not in self.child_actors:
                 new_channels[actor_id] = address
+            elif old_zb_address != new_zb_address:
+                logger.warning(
+                    "%s has changed its ZigBee address from %d to %d",
+                    actor_id,
+                    old_zb_address,
+                    new_zb_address,
+                )
+                new_channels[actor_id] = address
         gone_channels = []
-        for actor_id in self.child_actors:
-            if short_id(actor_id) not in channels:
+        for actor_id, info in self.child_actors.items():
+            instr_id = short_id(actor_id)
+            old_zb_address = info["route_to_instr"].zigbee_address
+            new_zb_address = channels[instr_id]
+            if instr_id not in channels:
+                gone_channels.append(actor_id)
+            elif old_zb_address != new_zb_address:
+                logger.warning(
+                    "%s has changed its ZigBee address from %d to %d",
+                    actor_id,
+                    old_zb_address,
+                    new_zb_address,
+                )
                 gone_channels.append(actor_id)
         if gone_channels:
             logger.info("Instruments removed from ZigBee: %s", gone_channels)

commit 7abbbf5a197e0553fe698b5ba630d72e296a5d09

!: regression introduced in eedaad8a4c #384

src/regserver/base_actor.py
@@ -204,7 +204,6 @@ class BaseActor(ActorTypeDispatcher):
         """
         if not is_flag_set()[0]:
             return
-        logger.warning("%s for %s from %s", msg, self.my_id, sender)
         if isinstance(
             msg.deadMessage,
             (
@@ -223,13 +222,14 @@ class BaseActor(ActorTypeDispatcher):
             ),
         ):
             self.send(self.registrar, UnsubscribeMsg(msg.deadAddress))
-            logger.info("The above warning can safely be ignored.")
         elif isinstance(msg.deadMessage, KeepAliveMsg):
             self.send(
                 msg.deadMessage.parent.parent_address,
                 DeadChildMsg(msg.deadMessage.child),
             )
+            logger.warning("%s for %s from %s", msg, self.my_id, sender)
         else:
+            logger.critical("%s for %s from %s", msg, self.my_id, sender)
             logger.critical("%s -> Emergency shutdown", self.my_id)
             system_shutdown()
 

src/regserver/modules/backend/mdns/mdns_listener.py
@@ -265,7 +265,7 @@ class MdnsListener(ServiceListener):
             device_id = self.device_id(name)
             device_actor = get_actor(self.registrar, device_id)
             if device_actor is None:
-                logger.warning("Actor %s does not exist.", device_id)
+                logger.debug("Actor %s does not exist.", device_id)
             else:
                 logger.debug("Kill device actor %s", device_id)
                 ActorSystem().tell(device_actor, KillMsg())

src/regserver/modules/backend/usb/net_usb_actor.py
@@ -149,7 +149,10 @@ class NetUsbActor(UsbActor):
         for actor_id, info in self.child_actors.items():
             instr_id = short_id(actor_id)
             old_zb_address = info["route_to_instr"].zigbee_address
-            new_zb_address = channels[instr_id]
+            if channels and channels.get(instr_id, False):
+                new_zb_address = channels[instr_id]
+            else:
+                new_zb_address = old_zb_address
             if instr_id not in channels:
                 gone_channels.append(actor_id)
             elif old_zb_address != new_zb_address: