SARAD-GmbH/RegServer

commit 99b0c5b7bb4ddce6454591044fcfaec8e477851d

!: killing residual processes didn't work on Linux
?: Threading in Redirector Actor is leading to the accumulation of residual
processes. These are only cleared during shutdown of the Actor System. #377

src/regserver/redirect_actor.py
@@ -11,8 +11,8 @@ socket as actor messages to the device actor.
 """
 import select
 import socket
-import time
 from threading import Thread
+from time import sleep
 
 from overrides import overrides  # type: ignore
 
@@ -32,12 +32,10 @@ class RedirectorActor(BaseActor):
         self._client_socket = None
         self._socket_info = None
         self.conn = None
-        self._host = config["MY_IP"]
-        self._port = None
+        self._address = (config["MY_IP"], None)
         self.read_list = None
         self.socket_loop_thread = Thread(
             target=self._loop,
-            daemon=True,
         )
 
     def receiveMsg_WakeupMessage(self, msg, _sender):
@@ -47,6 +45,7 @@ class RedirectorActor(BaseActor):
             self._loop()
 
     def _loop(self):
+        logger.info("Redirector thread in %s initialized", self.my_id)
         while not self.on_kill:
             try:
                 # Listen to socket and redirect any message from the socket to the device actor
@@ -64,33 +63,50 @@ class RedirectorActor(BaseActor):
                         self._cmd_handler()
             except (ValueError, IOError) as exception:
                 logger.error("%s in _loop function of redirector", exception)
+        self.read_list[0].close()
+        logger.info("Socket at port %d closed.", self._address[1])
+        logger.info("Finish socket_loop_thread")
 
     @overrides
     def receiveMsg_SetupMsg(self, msg, sender):
         logger.debug("Setup redirector actor")
         super().receiveMsg_SetupMsg(msg, sender)
-        for self._port in rest_frontend_config["PORT_RANGE"]:
-            try:
-                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server_socket.bind((self._host, self._port))
-                self._port = server_socket.getsockname()[1]
-                break
-            except OSError:
+        for port in rest_frontend_config["PORT_RANGE"]:
+            self._address = (self._address[0], port)
+            for res in socket.getaddrinfo(
+                self._address[0],
+                port,
+                socket.AF_UNSPEC,
+                socket.SOCK_STREAM,
+                0,
+                socket.AI_PASSIVE,
+            ):
+                af, socktype, proto, _canonname, sa = res
                 try:
-                    server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-                    server_socket.bind((self._host, self._port))
-                    self._port = server_socket.getsockname()[1]
-                    break
+                    server_socket = socket.socket(af, socktype, proto)
+                    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 except OSError as exception:
-                    logger.error("Cannot use port %d. %s", self._port, exception)
+                    logger.error("Cannot use port %d. %s", self._address[1], exception)
+                    server_socket = None
+                    self._address = (self._address[0], None)
+                    continue
+                try:
+                    server_socket.bind(sa)
+                    server_socket.listen(1)
+                except OSError as exception:
+                    logger.error(
+                        "Cannot listen on port %d. %s", self._address[1], exception
+                    )
                     server_socket.close()
-        try:
-            server_socket.listen()  # listen(5) maybe???
-        except OSError:
-            self._port = None
+                    server_socket = None
+                    self._address = (self._address[0], None)
+                    continue
+                break
+            if server_socket is not None:
+                break
         logger.debug("Server socket: %s", server_socket)
         self.read_list = [server_socket]
-        if self._port is None:
+        if self._address[1] is None:
             logger.critical(
                 "Cannot open socket in the configured port range %s",
                 rest_frontend_config["PORT_RANGE"],
@@ -99,26 +115,19 @@ class RedirectorActor(BaseActor):
         elif self.my_parent is None:
             self.my_parent = sender
             return_msg = SocketMsg(
-                ip_address=self._host, port=self._port, status=Status.OK
+                ip_address=self._address[0], port=self._address[1], status=Status.OK
             )
         else:
             logger.debug("my_parent: %s", self.my_parent)
             return_msg = SocketMsg(
-                ip_address=self._host, port=self._port, status=Status.OK_SKIPPED
+                ip_address=self._address[0],
+                port=self._address[1],
+                status=Status.OK_SKIPPED,
             )
         logger.debug("Setup finished with %s", return_msg)
         self.send(sender, return_msg)
         logger.debug("Start socket loop")
         self.socket_loop_thread.start()
-        logger.info("Redirector thread in %s initialized", self.my_id)
-
-    @overrides
-    def _kill_myself(self, register=True, resurrect=False):
-        try:
-            self.read_list[0].close()
-        except (ValueError, IOError) as exception:
-            logger.error("%s in KillMsg handler of redirector", exception)
-        super()._kill_myself(register=register, resurrect=resurrect)
 
     def _cmd_handler(self):
         """Handle a binary SARAD command received via the socket."""
@@ -129,7 +138,7 @@ class RedirectorActor(BaseActor):
             except (ConnectionResetError, BrokenPipeError):
                 logger.error("Connection reset by SARAD application software.")
                 data = None
-                time.sleep(5)
+                sleep(5)
             except (ValueError, IOError) as exception:
                 logger.error("%s in _sendall function", exception)
         if data is None:
@@ -157,7 +166,7 @@ class RedirectorActor(BaseActor):
                 return
             except (ConnectionResetError, BrokenPipeError):
                 logger.error("Connection reset by SARAD application software.")
-                time.sleep(5)
+                sleep(5)
             except (ValueError, IOError) as exception:
                 logger.error("%s in RxBinaryMsg handler", exception)
         logger.critical("Application software seems to be dead.")

src/regserver/main.py
@@ -207,7 +207,7 @@ def kill_residual_processes(end_with_error=True):
     if end_with_error:
         logger.info("Trying to kill residual processes. Fingers crossed!")
     if os.name == "posix":
-        process_regex = "python.sarad_registration_server"
+        process_regex = "python.+sarad_registration_server"
     elif os.name == "nt":
         process_regex = "regserver-service.exe"
     else:
@@ -313,14 +313,14 @@ def main():
             logger.info("Log entries go to %s", LOGFILENAME)
     except Exception:  # pylint: disable=broad-except
         logger.error("Initialization of log file failed.")
-    # maybe there are processes left from last run
-    kill_residual_processes(end_with_error=False)
     threading.excepthook = custom_hook
     if len(sys.argv) < 2:
         start_stop = "start"
     else:
         start_stop = sys.argv[1]
     if start_stop == "start":
+        # maybe there are processes left from last run
+        kill_residual_processes(end_with_error=False)
         set_file_flag(True)
         startup_tupel = startup()
         if not startup_tupel:

src/regserver/shutdown.py
@@ -91,14 +91,18 @@ def kill_processes(regex):
     if os.name == "posix":
         try:
             my_pid = os.getpid()
+            logger.info("My pid is %s", my_pid)
             pids = []
-            for line in os.popen("ps ax | grep " + regex + " | grep -v grep"):
+            for line in os.popen(
+                "ps ax | grep -E -i " + regex + " | grep -v -E 'grep|pdm'"
+            ):
                 fields = line.split()
                 pid = int(fields[0])
                 if pid != my_pid:
                     pids.append(pid)
             pids.sort(reverse=True)
             for pid in pids:
+                logger.info("Killing pid %s", pid)
                 os.kill(pid, signal.SIGKILL)
             return None
         except Exception as exception:  # pylint: disable=broad-except

commit cc3ef37a37c460b31c1861650352e7b9ccfb93c0

!: regression of 6fc8fed8da together with 99b0c5b7bb #410

src/regserver/modules/device_actor.py
@@ -379,12 +379,8 @@ class DeviceBaseActor(BaseActor):
         # pylint: disable=invalid-name
         """Handler for SocketMsg from Redirector Actor."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
-        try:
-            assert msg.status in (Status.OK, Status.OK_SKIPPED)
-        except AssertionError:
-            self.return_message = ReservationStatusMsg(
-                self.instr_id, Status.UNKNOWN_PORT
-            )
+        if msg.status not in (Status.OK, Status.OK_SKIPPED):
+            self.return_message = ReservationStatusMsg(self.instr_id, msg.status)
             self._send_reservation_status_msg()
             logger.debug("_send_reservation_status_msg case E")
             return

src/regserver/redirect_actor.py
@@ -71,6 +71,7 @@ class RedirectorActor(BaseActor):
     def receiveMsg_SetupMsg(self, msg, sender):
         logger.debug("Setup redirector actor")
         super().receiveMsg_SetupMsg(msg, sender)
+        success = False
         for port in rest_frontend_config["PORT_RANGE"]:
             self._address = (self._address[0], port)
             for res in socket.getaddrinfo(
@@ -93,6 +94,8 @@ class RedirectorActor(BaseActor):
                 try:
                     server_socket.bind(sa)
                     server_socket.listen(1)
+                    success = True
+                    break
                 except OSError as exception:
                     logger.error(
                         "Cannot listen on port %d. %s", self._address[1], exception
@@ -100,13 +103,11 @@ class RedirectorActor(BaseActor):
                     server_socket.close()
                     server_socket = socket.socket()
                     self._address = (self._address[0], 0)
-                    continue
-                break
-            if server_socket is not None:
+            if success:
                 break
         logger.debug("Server socket: %s", server_socket)
         self.read_list = [server_socket]
-        if not self._address[1]:
+        if not success:
             logger.critical(
                 "Cannot open socket in the configured port range %s",
                 rest_frontend_config["PORT_RANGE"],
@@ -123,12 +124,13 @@ class RedirectorActor(BaseActor):
             return_msg = SocketMsg(
                 ip_address=self._address[0],
                 port=self._address[1],
-                status=Status.OK_SKIPPED,
+                status=Status.OK,
             )
-        logger.debug("Setup finished with %s", return_msg)
+        logger.debug("Setup of %s finished with %s", self.my_id, return_msg)
         self.send(sender, return_msg)
-        logger.debug("Start socket loop")
-        self.socket_loop_thread.start()
+        if return_msg.status in (Status.OK, Status.OK_SKIPPED):
+            logger.debug("Start socket loop")
+            self.socket_loop_thread.start()
 
     def _cmd_handler(self):
         """Handle a binary SARAD command received via the socket."""

src/regserver/restapi.py
@@ -516,6 +516,7 @@ class ReserveDevice(Resource):
         if status in (
             Status.NOT_FOUND,
             Status.IS_NOT_FOUND,
+            Status.UNKNOWN_PORT,
         ):
             return {
                 "Error code": status.value,