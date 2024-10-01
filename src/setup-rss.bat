be224ec89df5362fddfb1e77ffa7f5c640af8a47
Michael Strey <strey@sarad.de>
2024-10-01 16:04:14 +0200

    !: trying to fix service restart #423

--- src/setup-rss.bat
+++ src/setup-rss.bat
@@ -1,7 +1,7 @@
 cd /D "%~dp0
 .\regserver-service.exe install
 sc.exe config SaradRegistrationServer start= auto type= own obj= "NT AUTHORITY\LocalService" password= "0123_Kennwort"
-sc.exe failure SaradRegistrationServer reset= 60 actions= restart/5000
+sc.exe failure SaradRegistrationServer reset= 60 actions= restart/5000/restart/5000/restart/5000
 sc.exe failureflag SaradRegistrationServer 1
 .\regserver-service.exe start
 Powershell.exe -executionpolicy remotesigned -File "%~dp0add-firewall-rule.ps1"
