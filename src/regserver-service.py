d9c655b3480a17fec29279db4f36591c8534d26e
Michael Strey <strey@sarad.de>
2024-10-07 14:45:51 +0200

    !: trying to fix #425

--- src/regserver-service.py
+++ src/regserver-service.py
@@ -37,6 +37,37 @@ class SaradRegistrationServer(win32serviceutil.ServiceFramework):
         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
         socket.setdefaulttimeout(60)
 
+    # Override the base class so we can accept additional events.
+    def GetAcceptedControls(self):
+        # say we accept them all.
+        rc = win32serviceutil.ServiceFramework.GetAcceptedControls(self)
+        rc |= (
+            win32service.SERVICE_ACCEPT_PRESHUTDOWN
+            | win32service.SERVICE_ACCEPT_POWEREVENT
+        )
+        return rc
+
+    # All extra events are sent via SvcOtherEx (SvcOther remains as a
+    # function taking only the first args for backwards compat)
+    def SvcOtherEx(self, control, event_type, data):
+        # This is only showing a few of the extra events - see the MSDN
+        # docs for "HandlerEx callback" for more info.
+        if control == win32service.SERVICE_ACCEPT_PRESHUTDOWN:
+            msg = f"Preshutdown event: code={control}, type={event_type}, data={data}"
+            self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+            win32event.SetEvent(self.hWaitStop)
+            system_shutdown(with_error=False)
+        elif control == win32service.SERVICE_CONTROL_POWEREVENT:
+            msg = f"Power event: setting {data}"
+        else:
+            msg = f"Other event: code={control}, type={event_type}, data={data}"
+
+        servicemanager.LogMsg(
+            servicemanager.EVENTLOG_INFORMATION_TYPE,
+            0xF000,  #  generic message
+            (msg, ""),
+        )
+
     def SvcStop(self):
         """Function that will be performed on 'service stop'.
 
