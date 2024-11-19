SARAD-GmbH/RegServer

commit b7acd893f3ef9cf35828273d40df5a91342c2ae2

!: 3 measures to cope with #431

src/regserver/base_actor.py
@@ -18,15 +18,16 @@ Actors created in the actor system
 """
 
 from overrides import overrides  # type: ignore
-from thespian.actors import (ActorAddress, ActorExitRequest,  # type: ignore
-                             ActorTypeDispatcher, ChildActorExited)
+from thespian.actors import ActorExitRequest  # type: ignore
+from thespian.actors import ActorAddress, ActorTypeDispatcher, ChildActorExited
 
 from regserver.actor_messages import (ActorType, DeadChildMsg,
                                       GetDeviceStatusMsg, KeepAliveMsg,
-                                      KillMsg, Parent, RescanAckMsg,
-                                      ReservationStatusMsg, RxBinaryMsg,
-                                      SetDeviceStatusMsg, SetupMdnsActorMsg,
-                                      SetupMsg, SetupUsbActorMsg, SubscribeMsg,
+                                      KillMsg, MqttReceiveMsg, Parent,
+                                      RescanAckMsg, ReservationStatusMsg,
+                                      RxBinaryMsg, SetDeviceStatusMsg,
+                                      SetupMdnsActorMsg, SetupMsg,
+                                      SetupUsbActorMsg, SubscribeMsg,
                                       SubscribeToActorDictMsg,
                                       SubscribeToDeviceStatusMsg,
                                       UnSubscribeFromActorDictMsg,
@@ -222,7 +223,7 @@ class BaseActor(ActorTypeDispatcher):
             ),
         ):
             self.send(self.registrar, UnsubscribeMsg(msg.deadAddress))
-        elif isinstance(msg.deadMessage, KeepAliveMsg):
+        elif isinstance(msg.deadMessage, KeepAliveMsg, MqttReceiveMsg):
             self.send(
                 msg.deadMessage.parent.parent_address,
                 DeadChildMsg(msg.deadMessage.child),

src/regserver/config.py
@@ -180,7 +180,7 @@ try:
         customization = tomlkit.load(custom_file)
 except OSError:
     customization = tomlkit.document()
-PING_FILE_NAME = f"{dirs.user_runtime_dir}{os.sep}ping"
+PING_FILE_NAME = f"{home}{os.sep}ping"
 os.makedirs(os.path.dirname(PING_FILE_NAME), exist_ok=True)
 FRMT = "%Y-%m-%dT%H:%M:%S"
 

src/regserver/modules/backend/mqtt/mqtt_base_actor.py
@@ -62,7 +62,7 @@ class MqttBaseActor(BaseActor):
 
         Returns:
             True, if MQTT client shall be started as clean session"""
-        if (Backend.MQTT in backend_config) and (Frontend.MQTT not in frontend_config):
+        if (Backend.MQTT in backend_config) or (Frontend.MQTT in frontend_config):
             logger.info("Checking %s", PING_FILE_NAME)
             try:
                 with open(PING_FILE_NAME, encoding="utf8") as pingfile:

src/regserver/modules/device_actor.py
@@ -616,26 +616,28 @@ class DeviceBaseActor(BaseActor):
     def _publish_status_change(self):
         """Publish a changed device status to all subscribers."""
         device_status = self.device_status
-        device_status["Identification"]["IS Id"] = self.device_status[
-            "Identification"
-        ].get("IS Id", config["IS_ID"])
-        for actor_address in self.subscribers.values():
-            self.send(
-                actor_address,
-                UpdateDeviceStatusMsg(self.my_id, device_status),
-            )
+        if device_status.get("Identification", False):
+            device_status["Identification"]["IS Id"] = self.device_status[
+                "Identification"
+            ].get("IS Id", config["IS_ID"])
+            for actor_address in self.subscribers.values():
+                self.send(
+                    actor_address,
+                    UpdateDeviceStatusMsg(self.my_id, device_status),
+                )
 
     def _publish_status(self, new_subscribers: list):
         """Publish a device status to all new_subscribers."""
         device_status = self.device_status
-        device_status["Identification"]["IS Id"] = self.device_status[
-            "Identification"
-        ].get("IS Id", config["IS_ID"])
-        for actor_address in new_subscribers:
-            self.send(
-                actor_address,
-                UpdateDeviceStatusMsg(self.my_id, device_status),
-            )
+        if device_status.get("Identification", False):
+            device_status["Identification"]["IS Id"] = self.device_status[
+                "Identification"
+            ].get("IS Id", config["IS_ID"])
+            for actor_address in new_subscribers:
+                self.send(
+                    actor_address,
+                    UpdateDeviceStatusMsg(self.my_id, device_status),
+                )
 
     def _request_start_monitoring_at_is(self, start_time=None, confirm=False):
         # pylint: disable=unused-argument

commit 1aef1ca091718e535a6724471834615e7e99673a

!: exception due to MQTT message storm #435
A regression was introduced with b7acd893f3 (#431).
During startup of MQTT we might receive a lot of MQTT messages from the last few
hours causing the creation and killing of Actors in a very fast sequence. This
might lead to DeadMessages.

src/regserver/base_actor.py
@@ -24,10 +24,10 @@ from thespian.actors import ActorAddress, ActorTypeDispatcher, ChildActorExited
 from regserver.actor_messages import (ActorType, DeadChildMsg,
                                       GetDeviceStatusMsg, KeepAliveMsg,
                                       KillMsg, MqttReceiveMsg, Parent,
-                                      RescanAckMsg, ReservationStatusMsg,
-                                      RxBinaryMsg, SetDeviceStatusMsg,
-                                      SetupMdnsActorMsg, SetupMsg,
-                                      SetupUsbActorMsg, SubscribeMsg,
+                                      PrepareMqttActorMsg, RescanAckMsg,
+                                      ReservationStatusMsg, RxBinaryMsg,
+                                      SetDeviceStatusMsg, SetupMdnsActorMsg,
+                                      SetupMsg, SetupUsbActorMsg, SubscribeMsg,
                                       SubscribeToActorDictMsg,
                                       SubscribeToDeviceStatusMsg,
                                       UnSubscribeFromActorDictMsg,
@@ -220,10 +220,12 @@ class BaseActor(ActorTypeDispatcher):
                 ReservationStatusMsg,
                 RxBinaryMsg,
                 RescanAckMsg,
+                MqttReceiveMsg,
+                PrepareMqttActorMsg,
             ),
         ):
             self.send(self.registrar, UnsubscribeMsg(msg.deadAddress))
-        elif isinstance(msg.deadMessage, (KeepAliveMsg, MqttReceiveMsg)):
+        elif isinstance(msg.deadMessage, (KeepAliveMsg)):
             self.send(
                 msg.deadMessage.parent.parent_address,
                 DeadChildMsg(msg.deadMessage.child),