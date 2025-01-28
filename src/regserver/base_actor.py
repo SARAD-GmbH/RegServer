SARAD-GmbH/RegServer

commit 34b8ec0fe1512358577be6bac82275c9d7c0e49f

!: make inner watchdog more relieable #433

src/regserver/base_actor.py
@@ -128,19 +128,7 @@ class BaseActor(ActorTypeDispatcher):
         # pylint: disable=invalid-name, unused-argument
         """Handler for KeepAliveMsg from the Registrar"""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
-        self._keep_alive_handler(msg.report)
-
-    def _keep_alive_handler(self, report):
-        if self.child_actors and not self.on_kill:
-            for child_id, child_actor in self.child_actors.items():
-                keep_alive_msg = KeepAliveMsg(
-                    parent=Parent(self.my_id, self.myAddress),
-                    child=child_id,
-                    report=report,
-                )
-                logger.debug("Forward %s to %s", keep_alive_msg, child_id)
-                self.send(child_actor["actor_address"], keep_alive_msg)
-        if report:
+        if msg.report:
             self._subscribe(True)
 
     def receiveMsg_UpdateActorDictMsg(self, msg, sender):

src/regserver/actor_messages.py
@@ -376,19 +376,15 @@ class Parent:
 
 @dataclass
 class KeepAliveMsg:
-    """Message sent to an actor from its parent actor in order to check
+    """Message sent to an actor from the Registrar Actor in order to check
     whether the actor is still existing.
 
     The Actor shall send a SubscribeMsg to the Registrar, if `report` is True.
 
     Args:
-        parent (Parent): Actor Id and address of the parent Actor sending the KeepAliveMsg.
-        child (str): Actor Id of the child Actor the KeepAliveMsg is addressed to.
         report (bool): True, if the child shall send a SubscribeMsg to Registrar.
     """
 
-    parent: Parent
-    child: str
     report: bool
 
 

src/regserver/registrar.py
@@ -21,11 +21,12 @@ from sarad.global_helpers import decode_instr_id
 
 from regserver.actor_messages import (ActorType, Backend, BaudRateAckMsg,
                                       BaudRateMsg, Frontend, GetHostInfoMsg,
-                                      HostInfoMsg, KillMsg, MqttConnectMsg,
-                                      PrepareMqttActorMsg, RescanAckMsg,
-                                      RescanMsg, ReturnDeviceActorMsg,
-                                      SetRtcAckMsg, SetRtcMsg, ShutdownAckMsg,
-                                      ShutdownMsg, StartMonitoringAckMsg,
+                                      HostInfoMsg, KeepAliveMsg, KillMsg,
+                                      MqttConnectMsg, PrepareMqttActorMsg,
+                                      RescanAckMsg, RescanMsg,
+                                      ReturnDeviceActorMsg, SetRtcAckMsg,
+                                      SetRtcMsg, ShutdownAckMsg, ShutdownMsg,
+                                      StartMonitoringAckMsg,
                                       StartMonitoringMsg, Status,
                                       StopMonitoringAckMsg, StopMonitoringMsg,
                                       UpdateActorDictMsg,
@@ -120,7 +121,10 @@ class Registrar(BaseActor):
                     timedelta(seconds=actor_config["KEEPALIVE_INTERVAL"]),
                     payload="keep alive",
                 )
-            self._keep_alive_handler(CHECK)
+            for actor_id in self.actor_dict:
+                self.send(
+                    self.actor_dict[actor_id]["address"], KeepAliveMsg(report=CHECK)
+                )
         elif msg.payload == "check":
             success = True
             for actor_id in self.actor_dict:

commit a5bac22185c3efb5620c612b7719a59664d9efdb

!: regression introduced with 34b8ec0fe1

src/regserver/base_actor.py
@@ -220,10 +220,6 @@ class BaseActor(ActorTypeDispatcher):
         ):
             self.send(self.registrar, UnsubscribeMsg(msg.deadAddress))
         elif isinstance(msg.deadMessage, (KeepAliveMsg)):
-            self.send(
-                msg.deadMessage.parent.parent_address,
-                DeadChildMsg(msg.deadMessage.child),
-            )
             logger.warning("%s for %s from %s", msg, self.my_id, sender)
         else:
             logger.critical("%s for %s from %s", msg, self.my_id, sender)