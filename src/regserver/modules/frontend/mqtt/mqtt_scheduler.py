SARAD-GmbH/RegServer

commit 5d6344fd2c3c0d165b17c3453fc94136d5c48c22

!: Reduce the noise...
There have been to many messages in the context of reservation and freeing of
reserved instruments.

src/regserver/modules/frontend/mqtt/mqtt_scheduler.py
@@ -280,7 +280,22 @@ class MqttSchedulerActor(MqttBaseActor):
         self._update_device_status(msg.device_id, msg.device_status)
 
     def _update_device_status(self, device_id, device_status):
+        reservation = device_status.get("Reservation", {})
+        reservation_object = Reservation(
+            timestamp=reservation.get("timestamp", time.time()),
+            active=reservation.get("Active", False),
+            host=reservation.get("Host", ""),
+            app=reservation.get("App", ""),
+            user=reservation.get("User", ""),
+            status=Status.OK,
+        )
+        if self.reservations.get(device_id) and (
+            reservation_object == self.reservations.get(device_id)
+        ):
+            logger.debug("No need to update anything at %s", device_id)
+            return
         logger.info("Forward status of %s via MQTT frontend", device_id)
+        logger.debug("%s: %s", device_id, device_status)
         instr_id = short_id(device_id)
         new_instrument_connected = False
         if not device_status.get("State", 2) < 2:

src/regserver/actor_messages.py
@@ -113,12 +113,20 @@ class TransportTechnology(IntEnum):
         return longform[self.value]
 
 
-BACKEND_TRANSLATOR = {
+TT_TRANSLATOR = {
     "local": TransportTechnology.LOCAL,
     "is1": TransportTechnology.IS1,
     "mdns": TransportTechnology.LAN,
     "mqtt": TransportTechnology.MQTT,
     "zigbee": TransportTechnology.LOCAL,
+}
+
+
+BACKEND_TRANSLATOR = {
+    "local": TransportTechnology.LOCAL,
+    "is1": TransportTechnology.IS1,
+    "lan": TransportTechnology.LAN,
+    "mqtt": TransportTechnology.MQTT,
 }
 
 

src/regserver/helpers.py
@@ -15,13 +15,12 @@ from datetime import timedelta
 
 from thespian.actors import ActorSystem  # type: ignore
 
-from regserver.actor_messages import (BACKEND_TRANSLATOR, ActorType,
-                                      FreeDeviceMsg, GetActorDictMsg,
-                                      GetDeviceStatusesMsg, GetDeviceStatusMsg,
-                                      GetHostInfoMsg, HostInfoMsg,
-                                      ReservationStatusMsg, ReserveDeviceMsg,
-                                      Status, TransportTechnology,
-                                      UpdateActorDictMsg,
+from regserver.actor_messages import (TT_TRANSLATOR, ActorType, FreeDeviceMsg,
+                                      GetActorDictMsg, GetDeviceStatusesMsg,
+                                      GetDeviceStatusMsg, GetHostInfoMsg,
+                                      HostInfoMsg, ReservationStatusMsg,
+                                      ReserveDeviceMsg, Status,
+                                      TransportTechnology, UpdateActorDictMsg,
                                       UpdateDeviceStatusesMsg,
                                       UpdateDeviceStatusMsg)
 from regserver.logger import logger
@@ -152,7 +151,7 @@ def transport_technology(device_id: str) -> TransportTechnology:
 
     """
     tech_str = device_id.split(".", 2)[-1]
-    return BACKEND_TRANSLATOR.get(tech_str, TransportTechnology.NONE)
+    return TT_TRANSLATOR.get(tech_str, TransportTechnology.NONE)
 
 
 def find(pattern, path):

src/regserver/modules/backend/mdns/device_actor.py
@@ -46,7 +46,7 @@ class TimeoutHTTPAdapter(HTTPAdapter):
     def send(
         self, request, stream=False, timeout=None, verify=True, cert=None, proxies=None
     ):
-        # pylint: disable=too-many-arguments
+        # pylint: disable=too-many-arguments, too-many-positional-arguments
         if timeout is None:
             timeout = self.timeout
         return super().send(request, stream, timeout, verify, cert, proxies)
@@ -367,7 +367,7 @@ class DeviceActor(DeviceBaseActor):
             self.device_status["Reservation"] = self.response[self.device_id][
                 "Reservation"
             ]
-        except Exception as exception:
+        except Exception as exception:  # pylint: disable=broad-exception-caught
             logger.info("Exception in _finish_free: %s", exception)
             self.device_status["Reservation"] = {}
         if self.success in (Status.NOT_FOUND, Status.IS_NOT_FOUND):
@@ -420,7 +420,7 @@ class DeviceActor(DeviceBaseActor):
                 self.device_status["Reservation"] = self.response[self.device_id][
                     "Reservation"
                 ]
-            except Exception as exception:
+            except Exception as exception:  # pylint: disable=broad-exception-caught
                 logger.info("Exception in _finish_reserve: %s", exception)
                 self.device_status["Reservation"] = {}
         self.return_message = ReservationStatusMsg(
@@ -463,7 +463,9 @@ class DeviceActor(DeviceBaseActor):
             super().receiveMsg_SetDeviceStatusMsg(msg, sender)
             self.occupied = False
             if self.device_status:
-                self.device_status["Reservation"] = msg.device_status.get("Reservation")
+                self.device_status["Reservation"] = msg.device_status.get(
+                    "Reservation", self.device_status["Reservation"]
+                )
             else:
                 self.device_status = msg.device_status
             logger.debug("Device status: %s", self.device_status)
@@ -510,30 +512,25 @@ class DeviceActor(DeviceBaseActor):
                 )
                 self.device_status.pop("Reservation", None)
             else:
-                if reservation.get("Active", False):
-                    using_host = reservation.get("Host", "")
-                    if self.reserve_device_msg is not None:
-                        my_host = self.reserve_device_msg.host
-                    else:
-                        my_host = config["MY_HOSTNAME"]
-                    if compare_hostnames(using_host, my_host):
-                        logger.debug(
-                            "Occupied by me. Using host is %s, my host is %s",
-                            using_host,
-                            my_host,
-                        )
-                    else:
-                        logger.debug("Occupied by somebody else.")
-                        logger.debug("Using host: %s, my host: %s", using_host, my_host)
-                        self.occupied = True
-                        reservation.pop("IP", None)
-                        reservation.pop("Port", None)
-                    self.wakeupAfter(
-                        timedelta(seconds=UPDATE_INTERVAL), payload="update"
-                    )
-                    self.device_status["Reservation"] = reservation
+                using_host = reservation.get("Host", "")
+                if self.reserve_device_msg is not None:
+                    my_host = self.reserve_device_msg.host
                 else:
-                    self.device_status.pop("Reservation", None)
+                    my_host = config["MY_HOSTNAME"]
+                if compare_hostnames(using_host, my_host):
+                    logger.debug(
+                        "Occupied by me. Using host is %s, my host is %s",
+                        using_host,
+                        my_host,
+                    )
+                else:
+                    logger.debug("Occupied by somebody else.")
+                    logger.debug("Using host: %s, my host: %s", using_host, my_host)
+                    self.occupied = True
+                    reservation.pop("IP", None)
+                    reservation.pop("Port", None)
+                self.wakeupAfter(timedelta(seconds=UPDATE_INTERVAL), payload="update")
+                self.device_status["Reservation"] = reservation
             self._publish_status_change()
         else:
             logger.info(

src/regserver/modules/device_actor.py
@@ -9,6 +9,7 @@
 
 """
 
+import copy
 from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
 
@@ -84,6 +85,7 @@ class DeviceBaseActor(BaseActor):
     def __init__(self):
         super().__init__()
         self.device_status: dict = {}
+        self.prev_device_status: dict = {}
         self.subscribers: dict = {}
         self.reserve_device_msg: ReserveDeviceMsg = ReserveDeviceMsg(
             host="", user="", app="", create_redirector=False
@@ -615,28 +617,29 @@ class DeviceBaseActor(BaseActor):
 
     def _publish_status_change(self):
         """Publish a changed device status to all subscribers."""
-        device_status = self.device_status
-        if device_status.get("Identification", False):
-            device_status["Identification"]["IS Id"] = self.device_status[
-                "Identification"
-            ].get("IS Id", config["IS_ID"])
-            for actor_address in self.subscribers.values():
-                self.send(
-                    actor_address,
-                    UpdateDeviceStatusMsg(self.my_id, device_status),
-                )
+        if self.prev_device_status != self.device_status:
+            if self.device_status.get("Identification", False):
+                self.device_status["Identification"]["IS Id"] = self.device_status[
+                    "Identification"
+                ].get("IS Id", config["IS_ID"])
+                for actor_address in self.subscribers.values():
+                    self.send(
+                        actor_address,
+                        UpdateDeviceStatusMsg(self.my_id, self.device_status),
+                    )
+            logger.debug("Published change: %s", self.device_status)
+            self.prev_device_status = copy.deepcopy(self.device_status)
 
     def _publish_status(self, new_subscribers: list):
         """Publish a device status to all new_subscribers."""
-        device_status = self.device_status
-        if device_status.get("Identification", False):
-            device_status["Identification"]["IS Id"] = self.device_status[
+        if self.device_status.get("Identification", False):
+            self.device_status["Identification"]["IS Id"] = self.device_status[
                 "Identification"
             ].get("IS Id", config["IS_ID"])
             for actor_address in new_subscribers:
                 self.send(
                     actor_address,
-                    UpdateDeviceStatusMsg(self.my_id, device_status),
+                    UpdateDeviceStatusMsg(self.my_id, self.device_status),
                 )
 
     def _request_start_monitoring_at_is(self, start_time=None, confirm=False):

commit d1f1b9552d45ed9b69146259b8a0beac05a73561

!: regression from 5d6344fd2c

src/regserver/modules/frontend/mqtt/mqtt_scheduler.py
@@ -268,7 +268,7 @@ class MqttSchedulerActor(MqttBaseActor):
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
         for device_id, status_dict in msg.device_statuses.items():
             if transport_technology(device_id) in mqtt_frontend_config["GATEWAY"]:
-                self._update_device_status(device_id, status_dict)
+                self._update_device_status(device_id, status_dict, force=True)
 
     def receiveMsg_UpdateDeviceStatusMsg(self, msg, sender):
         # pylint: disable=invalid-name, too-many-locals
@@ -277,9 +277,9 @@ class MqttSchedulerActor(MqttBaseActor):
         Adds a new instrument to the list of available instruments
         or updates the reservation state."""
         logger.debug("%s for %s from %s", msg, self.my_id, sender)
-        self._update_device_status(msg.device_id, msg.device_status)
+        self._update_device_status(msg.device_id, msg.device_status, force=False)
 
-    def _update_device_status(self, device_id, device_status):
+    def _update_device_status(self, device_id, device_status, force=False):
         reservation = device_status.get("Reservation", {})
         reservation_object = Reservation(
             timestamp=reservation.get("timestamp", time.time()),
@@ -289,8 +289,10 @@ class MqttSchedulerActor(MqttBaseActor):
             user=reservation.get("User", ""),
             status=Status.OK,
         )
-        if self.reservations.get(device_id) and (
-            reservation_object == self.reservations.get(device_id)
+        if (
+            self.reservations.get(device_id)
+            and (reservation_object == self.reservations.get(device_id))
+            and not force
         ):
             logger.debug("No need to update anything at %s", device_id)
             return
@@ -299,12 +301,16 @@ class MqttSchedulerActor(MqttBaseActor):
         instr_id = short_id(device_id)
         new_instrument_connected = False
         if not device_status.get("State", 2) < 2:
-            reservation = device_status.get("Reservation")
+            reservation = device_status.get("Reservation", {})
             self.mqttc.subscribe(f"{self.group}/{self.is_id}/{instr_id}/control", 2)
             self.mqttc.subscribe(f"{self.group}/{self.is_id}/{instr_id}/cmd", 2)
             identification = device_status["Identification"]
             identification["Host"] = self.is_meta.host
-            message = {"State": 2, "Identification": identification}
+            message = {
+                "State": 2,
+                "Identification": identification,
+                "Reservation": reservation,
+            }
             self.mqttc.publish(
                 topic=f"{self.group}/{self.is_id}/{instr_id}/meta",
                 payload=json.dumps(message),